25d062412f5e08409dca96f9533e2ffbd67019cc
David Liu <dliu@broadcom.com>
2014-08-13 09:42:17 -0700

    Changes for LRW38e
    Change-Id: Ie332643098d4742cb8debdebc1a847f5797699eb

--- bcm_platform/libaudio2/brcm_audio2.cpp
+++ bcm_platform/libaudio2/brcm_audio2.cpp
@@ -413,7 +413,8 @@ static int adev_open_output_stream(struct audio_hw_device *dev,
                                    audio_devices_t devices,
                                    audio_output_flags_t flags,
                                    struct audio_config *config,
-                                   struct audio_stream_out **stream_out)
+                                   struct audio_stream_out **stream_out,
+                                   const char *address __unused)
 {
     struct brcm_audio_device *adev = (struct brcm_audio_device *)dev;
     struct brcm_stream_out *out;
@@ -566,7 +567,9 @@ static int adev_open_input_stream(struct audio_hw_device *dev,
                                   audio_devices_t devices,
                                   struct audio_config *config,
                                   struct audio_stream_in **stream_in,
-                                  audio_input_flags_t flags __unused)
+                                  audio_input_flags_t flags __unused,
+                                  const char *address __unused,
+                                  audio_source_t source __unused)
 {
     struct brcm_audio_device *adev = (struct brcm_audio_device *)dev;
     struct brcm_stream_in *in;
