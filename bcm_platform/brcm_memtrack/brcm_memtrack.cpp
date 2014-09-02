a990434f7826fc21860e2a834072d3e60a6d0d3d
Pierre Couillaud <pierre@broadcom.com>
2014-09-02 13:23:30 -0700

    memtrack: initial HAL integration.
    reports null data for the time being, but does act
    properly as expected.
    
    refs #SWANDROID-944
    
    Change-Id: If71b5c6437bf050ffa98a3480ec97521a25f35d9
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- bcm_platform/brcm_memtrack/brcm_memtrack.cpp
+++ bcm_platform/brcm_memtrack/brcm_memtrack.cpp
@@ -1,6 +1,6 @@
 /******************************************************************************
  *    (c)2014 Broadcom Corporation
- * 
+ *
  * This program is the proprietary software of Broadcom Corporation and/or its licensors,
  * and may only be used, duplicated, modified or distributed pursuant to the terms and
  * conditions of a separate, written license agreement executed between you and Broadcom
@@ -9,49 +9,39 @@
  * Software, and Broadcom expressly reserves all rights in and to the Software and all
  * intellectual property rights therein.  IF YOU HAVE NO AUTHORIZED LICENSE, THEN YOU
  * HAVE NO RIGHT TO USE THIS SOFTWARE IN ANY WAY, AND SHOULD IMMEDIATELY
- * NOTIFY BROADCOM AND DISCONTINUE ALL USE OF THE SOFTWARE.  
- *  
+ * NOTIFY BROADCOM AND DISCONTINUE ALL USE OF THE SOFTWARE.
+ *
  * Except as expressly set forth in the Authorized License,
- *  
+ *
  * 1.     This program, including its structure, sequence and organization, constitutes the valuable trade
  * secrets of Broadcom, and you shall use all reasonable efforts to protect the confidentiality thereof,
  * and to use this information only in connection with your use of Broadcom integrated circuit products.
- *  
- * 2.     TO THE MAXIMUM EXTENT PERMITTED BY LAW, THE SOFTWARE IS PROVIDED "AS IS" 
- * AND WITH ALL FAULTS AND BROADCOM MAKES NO PROMISES, REPRESENTATIONS OR 
- * WARRANTIES, EITHER EXPRESS, IMPLIED, STATUTORY, OR OTHERWISE, WITH RESPECT TO 
- * THE SOFTWARE.  BROADCOM SPECIFICALLY DISCLAIMS ANY AND ALL IMPLIED WARRANTIES 
- * OF TITLE, MERCHANTABILITY, NONINFRINGEMENT, FITNESS FOR A PARTICULAR PURPOSE, 
- * LACK OF VIRUSES, ACCURACY OR COMPLETENESS, QUIET ENJOYMENT, QUIET POSSESSION 
- * OR CORRESPONDENCE TO DESCRIPTION. YOU ASSUME THE ENTIRE RISK ARISING OUT OF 
- * USE OR PERFORMANCE OF THE SOFTWARE.
- * 
- * 3.     TO THE MAXIMUM EXTENT PERMITTED BY LAW, IN NO EVENT SHALL BROADCOM OR ITS 
- * LICENSORS BE LIABLE FOR (i) CONSEQUENTIAL, INCIDENTAL, SPECIAL, INDIRECT, OR 
- * EXEMPLARY DAMAGES WHATSOEVER ARISING OUT OF OR IN ANY WAY RELATING TO YOUR 
- * USE OF OR INABILITY TO USE THE SOFTWARE EVEN IF BROADCOM HAS BEEN ADVISED OF 
- * THE POSSIBILITY OF SUCH DAMAGES; OR (ii) ANY AMOUNT IN EXCESS OF THE AMOUNT 
- * ACTUALLY PAID FOR THE SOFTWARE ITSELF OR U.S. $1, WHICHEVER IS GREATER. THESE 
- * LIMITATIONS SHALL APPLY NOTWITHSTANDING ANY FAILURE OF ESSENTIAL PURPOSE OF 
- * ANY LIMITED REMEDY.
  *
- * $brcm_Workfile: brcm_memtrack.cpp $
- * $brcm_Revision:  $
- * $brcm_Date: 7/19/14 4:22p $
- * $brcm_Author: ttrammel@broadcom.com
- * 
- * Module Description:
- * 
- * Revision History:
- * 
+ * 2.     TO THE MAXIMUM EXTENT PERMITTED BY LAW, THE SOFTWARE IS PROVIDED "AS IS"
+ * AND WITH ALL FAULTS AND BROADCOM MAKES NO PROMISES, REPRESENTATIONS OR
+ * WARRANTIES, EITHER EXPRESS, IMPLIED, STATUTORY, OR OTHERWISE, WITH RESPECT TO
+ * THE SOFTWARE.  BROADCOM SPECIFICALLY DISCLAIMS ANY AND ALL IMPLIED WARRANTIES
+ * OF TITLE, MERCHANTABILITY, NONINFRINGEMENT, FITNESS FOR A PARTICULAR PURPOSE,
+ * LACK OF VIRUSES, ACCURACY OR COMPLETENESS, QUIET ENJOYMENT, QUIET POSSESSION
+ * OR CORRESPONDENCE TO DESCRIPTION. YOU ASSUME THE ENTIRE RISK ARISING OUT OF
+ * USE OR PERFORMANCE OF THE SOFTWARE.
+ *
+ * 3.     TO THE MAXIMUM EXTENT PERMITTED BY LAW, IN NO EVENT SHALL BROADCOM OR ITS
+ * LICENSORS BE LIABLE FOR (i) CONSEQUENTIAL, INCIDENTAL, SPECIAL, INDIRECT, OR
+ * EXEMPLARY DAMAGES WHATSOEVER ARISING OUT OF OR IN ANY WAY RELATING TO YOUR
+ * USE OF OR INABILITY TO USE THE SOFTWARE EVEN IF BROADCOM HAS BEEN ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGES; OR (ii) ANY AMOUNT IN EXCESS OF THE AMOUNT
+ * ACTUALLY PAID FOR THE SOFTWARE ITSELF OR U.S. $1, WHICHEVER IS GREATER. THESE
+ * LIMITATIONS SHALL APPLY NOTWITHSTANDING ANY FAILURE OF ESSENTIAL PURPOSE OF
+ * ANY LIMITED REMEDY.
   *****************************************************************************/
 #include <errno.h>
 #include "brcm_memtrack.h"
 
-#include "nexus_ipc_client_factory.h"
-#include "nexus_memory.h"
-
-extern "C" void *v3d_get_nexus_client_context(void);
+/* max number of record supported, which today corresponds
+ * to the max number of 'heap per type' supported.
+ */
+#define MEMTRACK_HAL_NUM_RECORDS_MAX 1
 
 int brcm_memtrack_init(const struct memtrack_module *module)
 {
@@ -64,18 +54,35 @@ int brcm_memtrack_get_memory(const struct memtrack_module *module,
                                 struct memtrack_record *records,
                                 size_t *num_records)
 {
-    NexusClientContext *nexus_client = NULL;
-    nexus_client = (reinterpret_cast<NexusClientContext *>(v3d_get_nexus_client_context()));
-    if (nexus_client)
-       LOGI("%s:%d recvd client_handle = %p", __FUNCTION__, __LINE__, (void *)nexus_client);
-    else
-       while(1) LOGE("%s:%d got NULL client handle", __FUNCTION__, __LINE__);
+   int rc = 0;
+   int i;
 
-    if (type == MEMTRACK_TYPE_GL || type == MEMTRACK_TYPE_GRAPHICS) {
-        return 0;
-    }
+   if (!*num_records) {
+       *num_records = MEMTRACK_HAL_NUM_RECORDS_MAX;
+       goto exit;
+   }
 
-    return -EINVAL;
+   switch(type) {
+       case MEMTRACK_TYPE_GL:
+       case MEMTRACK_TYPE_GRAPHICS:
+       case MEMTRACK_TYPE_CAMERA:
+       case MEMTRACK_TYPE_MULTIMEDIA:
+       case MEMTRACK_TYPE_OTHER:
+       break;
+
+       default:
+          rc = -EINVAL;
+          goto exit;
+   }
+
+
+   for (i = 0; i < *num_records; i++) {
+      records[i].size_in_bytes = 0;
+      records[i].flags = MEMTRACK_FLAG_SHARED | MEMTRACK_FLAG_DEDICATED | MEMTRACK_FLAG_NONSECURE;
+   }
+
+exit:
+    return rc;
 }
 
 static struct hw_module_methods_t memtrack_module_methods = {
@@ -88,7 +95,7 @@ struct memtrack_module HAL_MODULE_INFO_SYM = {
         module_api_version: MEMTRACK_MODULE_API_VERSION_0_1,
         hal_api_version: HARDWARE_HAL_API_VERSION,
         id: MEMTRACK_HARDWARE_MODULE_ID,
-        name: "Brcmstb Memory Tracker HAL",
+        name: "Memory Tracker HAL",
         author: "Broadcom Corp",
         methods: &memtrack_module_methods,
     },
