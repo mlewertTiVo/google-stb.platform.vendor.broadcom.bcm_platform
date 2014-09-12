0ab8aad027d0a59511df4b53bd823d311830f7c9
Pierre Couillaud <pierre@broadcom.com>
2014-09-11 18:17:49 -0700

    plat-droid.py: edit only, usage info.
    refs #SWANDROID-857
    
    Change-Id: Ieb340c9cccc1f7f5a4c598a8294c4e621dfc11d6
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- bcm_platform/tools/plat-droid.py
+++ bcm_platform/tools/plat-droid.py
@@ -68,7 +68,7 @@ def write_header(s, d):
 
 # how you should use this.
 def plat_droid_usage():
-	print 'usage: plat-droid.py <chip-number> <chip-rev> <board-type>'
+	print 'usage: plat-droid.py <platform> <chip-rev> <board-type>'
 	print '\t<platform>    - the BCM platform number to build for, eg: 97252, 97445, ...'
 	print '\t<chip-rev>    - the BCM chip revision of interest, eg: A0, B0, C1, ...'
 	print '\t<board-type>  - the target board type, eg: SV, C'
