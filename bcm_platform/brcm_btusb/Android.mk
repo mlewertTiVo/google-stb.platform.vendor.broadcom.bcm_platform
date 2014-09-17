72a72086fa805251f2c1fa9d0080770d551cf4b4
Reiko Nagano <rnagano@broadcom.com>
2014-09-16 20:40:02 -0400

    L: bluetooth USB changes from L-PDK refs #SWANDROID-922
    Change-Id: Ib94f0687387e44da14e5d581e12f44d03529ca48

--- bcm_platform/brcm_btusb/Android.mk
+++ bcm_platform/brcm_btusb/Android.mk
@@ -1,10 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE :=driver/btusb.ko
+LOCAL_MODULE := driver/btusb.ko
 LOCAL_MODULE_TAGS := optional debug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT)/vendor/broadcom/btusb
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
-
 include $(BUILD_PREBUILT)
