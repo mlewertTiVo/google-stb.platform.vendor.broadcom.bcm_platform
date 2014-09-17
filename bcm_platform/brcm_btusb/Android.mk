24942c74d07ce229959988fc27689907c0820a2b
Reiko Nagano <rnagano@broadcom.com>
2014-09-17 17:31:50 -0400

    L: add /dev/btusb0 refs #SWANDROID-922
    Change-Id: Ia15f9f0a42af46cb33f6ebedcd880c3ec1509b28

--- bcm_platform/brcm_btusb/Android.mk
+++ bcm_platform/brcm_btusb/Android.mk
@@ -7,3 +7,12 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT)/vendor/broadcom/btusb
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := bt_vendor.conf
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT)/etc/bluetooth
+LOCAL_MODULE_TAGS := optional debug
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
