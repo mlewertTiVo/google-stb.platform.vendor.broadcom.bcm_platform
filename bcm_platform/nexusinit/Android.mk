e18539a6f5aaca4115d71d9f8cf8145864b7c794
Pierre Couillaud <pierre@broadcom.com>
2014-10-15 11:53:24 -0700

    nexusinit: clean up of the nexus init code.
    - remove conditional code that we do not use nor test
    for that matter.
    - add 'wait for server to be ready' mechanism using nexus
    join rather than plain 'sleep'.
    - clean up code from non used paths.
    - moved kernel drivers under 'system/vendor/drivers'.
    
    refs #SWANDROID-1025
    
    Change-Id: I9542946c8c8510d033f9ac20df237ddcd58aa678
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- bcm_platform/nexusinit/Android.mk
+++ bcm_platform/nexusinit/Android.mk
@@ -12,74 +12,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 REFSW_PATH :=vendor/broadcom/bcm_platform/brcm_nexus
 LOCAL_PATH := $(call my-dir)
-APPLIBS_TOP ?=$(LOCAL_PATH)/../../../../../../../..
 NEXUS_TOP ?= $(LOCAL_PATH)/../../../../../../../../../nexus
-
-# Nexus multi-process, client-server related CFLAGS
-MP_CFLAGS = -DANDROID_CLIENT_SECURITY_MODE=$(ANDROID_CLIENT_SECURITY_MODE)
-
-ifeq ($(ANDROID_SUPPORTS_NXCLIENT),y)
-MP_CFLAGS += -DANDROID_SUPPORTS_NXCLIENT
-
-ifeq ($(NEXUS_MODE),proxy)
-NEXUS_LIB=libnexus
-else
-ifeq ($(NEXUS_WEBCPU),core1_server)
-NEXUS_LIB=libnexus_webcpu
-else
-NEXUS_LIB=libnexus_client
-endif
-endif
-
-else
-NEXUS_LIB=libnexus
-endif
-
-ifeq ($(ANDROID_UNDER_LXC),y)
-MP_CFLAGS += -DANDROID_UNDER_LXC=1
-else
-MP_CFLAGS += -DANDROID_UNDER_LXC=0
-endif
-
-ifeq ($(ANDROID_SUPPORTS_FRONTEND_SERVICE),y)
-MP_CFLAGS += -DANDROID_SUPPORTS_FRONTEND_SERVICE=1
-else
-MP_CFLAGS += -DANDROID_SUPPORTS_FRONTEND_SERVICE=0
-endif
-
-ifeq ($(ATP_BUILD),y)
-MP_CFLAGS += -DATP_BUILD=1
-else
-MP_CFLAGS += -DATP_BUILD=0
-endif
 
 include $(CLEAR_VARS)
 
-ifeq ($(ANDROID_ENABLE_REMOTEA),y)
-LOCAL_CFLAGS += -DANDROID_ENABLE_REMOTEA
-endif
+# Nexus multi-process, client-server related CFLAGS
+MP_CFLAGS = -DANDROID_CLIENT_SECURITY_MODE=$(ANDROID_CLIENT_SECURITY_MODE)
+MP_CFLAGS += -DANDROID_SUPPORTS_NXCLIENT
+MP_CFLAGS += -DANDROID_UNDER_LXC=0
+MP_CFLAGS += -DANDROID_SUPPORTS_FRONTEND_SERVICE=0
+MP_CFLAGS += -DATP_BUILD=0
 
 include $(NEXUS_TOP)/nxclient/include/nxclient.inc
-$(warning NX-BUILD: CFLAGS ${NEXUS_CFLAGS}, CLI-CFLAGS: ${NXCLIENT_CFLAGS}, CLI-INC: ${NXCLIENT_INCLUDES})
 
 LOCAL_PRELINK_MODULE := false
-LOCAL_SHARED_LIBRARIES := liblog libcutils libbinder libutils libnexusipcclient libnexusservice $(NEXUS_LIB)
+LOCAL_SHARED_LIBRARIES := liblog \
+                          libcutils \
+                          libbinder \
+                          libutils \
+                          libnexusipcclient \
+                          libnexusservice \
+                          libnexus \
+                          libnxclient
 
-ifeq ($(ANDROID_SUPPORTS_FRONTEND_SERVICE),y)
-LOCAL_SHARED_LIBRARIES += libnexusfrontendservice
-LOCAL_C_INCLUDES += $(REFSW_PATH)/../libnexusfrontendservice
-endif 
-
-LOCAL_C_INCLUDES += $(REFSW_PATH)/bin/include \
-                    $(REFSW_PATH)/../libnexusservice
+LOCAL_C_INCLUDES += $(REFSW_PATH)/../libnexusservice
 LOCAL_C_INCLUDES += $(REFSW_PATH)/../libnexusipc
-
-ifeq ($(ANDROID_SUPPORTS_NXCLIENT),y)
 LOCAL_C_INCLUDES += $(NXCLIENT_INCLUDES)
-endif
 
 LOCAL_SRC_FILES := \
                 nexusinit.cpp
@@ -88,9 +48,6 @@ LOCAL_MODULE := nexusinit
 LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += $(NEXUS_CFLAGS) $(addprefix -I,$(NEXUS_APP_INCLUDE_PATHS)) $(addprefix -D,$(NEXUS_APP_DEFINES)) -DANDROID $(MP_CFLAGS)
 LOCAL_CFLAGS += -DLOGD=ALOGD -DLOGE=ALOGE -DLOGW=ALOGW -DLOGV=ALOGV -DLOGI=ALOGI
-ifeq ($(SBS_USES_TRELLIS_INPUT_EVENTS),y)
-LOCAL_CFLAGS += -DSBS_USES_TRELLIS_INPUT_EVENTS
-endif
 ifeq ($(BCM_OMX_SUPPORT_ENCODER),y)
 LOCAL_CFLAGS += -DBCM_OMX_SUPPORT_ENCODER
 endif
--- bcm_platform/nexusinit/nexusinit.cpp
+++ bcm_platform/nexusinit/nexusinit.cpp
@@ -64,23 +64,17 @@
 #include <binder/ProcessState.h>
 
 #include "nexusinit.h"
-
+#include "nxclient.h"
 #include "nexusnxservice.h"
-#if ANDROID_SUPPORTS_FRONTEND_SERVICE
-#include "nexusfrontendservice.h"
-#endif
 
-#ifdef NEXUS_MODE_proxy
-#define NEXUS_DRIVER_FILENAME          "/system/etc/nexus.ko"
-#else
-#define NEXUS_DRIVER_FILENAME          "/system/etc/bcmdriver.ko"
-#endif
-#define NEXUS_IR_INPUT_DRIVER_FILENAME "/system/etc/nexus_ir_input_event.ko"
-#define EVENT_FORWARD_DRIVER_FILENAME  "/system/etc/event_forward.ko"
-#define EVENT_WRITE_DRIVER_FILENAME    "/system/etc/event_write.ko"
+#define VENDOR_DRIVER_PATH             "system/vendor/drivers"
+
+#define WAKEUP_DRIVER_FILENAME         "wakeup_drv.ko"
+#define NEXUS_DRIVER_FILENAME          "nexus.ko"
+#define NEXUS_IR_INPUT_DRIVER_FILENAME "nexus_ir_input_event.ko"
+#define NX_ASHMEM_DRIVER_FILENAME      "nx_ashmem.ko"
+
 #define NXSERVER_FILENAME              "/system/bin/nxserver"
-#define NX_ASHMEM_DRIVER_FILENAME      "/system/etc/nx_ashmem.ko"
-#define WAKEUP_DRIVER_FILENAME         "/system/etc/wakeup_drv.ko"
 
 extern "C" long int init_module(void *, unsigned long, const char*);
 extern "C" long int delete_module(const char *, unsigned long, unsigned long, unsigned long);
@@ -140,10 +134,14 @@ BERR_Code nexusinit_insmod(const char *filename, const char *args)
     long int ret;
     unsigned long len;
     void *file;
+    char full_name[256];
 
-    file = nexusinit_grab_file(filename, &len);
+    memset(full_name, 0, sizeof(full_name));
+    snprintf(full_name, sizeof(full_name), "%s/%s", VENDOR_DRIVER_PATH, filename);
+
+    file = nexusinit_grab_file(full_name, &len);
     if (!file) {
-        LOGE("nexusinit: nexusinit_grab_file returned NULL!");
+        LOGE("nexusinit: nexusinit_grab_file(%s) returned NULL!", full_name);
         return BERR_UNKNOWN;
     }
 
@@ -157,21 +155,8 @@ BERR_Code nexusinit_insmod(const char *filename, const char *args)
     return BERR_SUCCESS;
 }
 
-BERR_Code nexusinit_rmmod(const char *name)
-{
-    long int ret;
-
-    ret = delete_module(name,0,0,0);
-    if (ret != 0) {
-        return BERR_UNKNOWN;
-    }
-
-    return BERR_SUCCESS;
-}
-
 BERR_Code nexusinit_ir()
 {
-#ifdef NEXUS_MODE_proxy
     char module_arg[16];
     char value[PROPERTY_VALUE_MAX];
 
@@ -179,11 +164,7 @@ BERR_Code nexusinit_ir()
 
     /* Runtime check for remote type, default to silver remote */
     property_get("ro.ir_remote", value, NULL);
-
-    /* Don't insmod our IR driver, if we want to test the NxClient applications using IR R/C */
-#ifdef ANDROID_SUPPORTS_NXCLIENT
     if (strcmp(value, "nxclient") != 0)
-#endif
     {
         /* After the platform init, insmod the ir event driver */
         if (nexusinit_insmod(NEXUS_IR_INPUT_DRIVER_FILENAME, (strcmp(value,"remote_a")==0) ? "remote_a=1" : "") != BERR_SUCCESS) {
@@ -191,7 +172,6 @@ BERR_Code nexusinit_ir()
             return BERR_UNKNOWN;
         }
     }
-#endif
 
     /* Add the nx_ashmem module which is required for gralloc to function */
     if (nexusinit_insmod(NX_ASHMEM_DRIVER_FILENAME, "") != BERR_SUCCESS) {
@@ -229,9 +209,7 @@ void startNxServer(void)
 #ifndef BCM_OMX_SUPPORT_ENCODER
     strcat(cmdRunNxServer, "-transcode off ");
 #endif
-#if !ANDROID_SUPPORTS_FRONTEND_SERVICE
     strcat(cmdRunNxServer, "-frontend off ");
-#endif
     strcat(cmdRunNxServer, "&");
     ALOGI("NXSERVER CMD: %s",cmdRunNxServer);
     system(cmdRunNxServer);
@@ -245,6 +223,7 @@ int main(void)
     NEXUS_PlatformSettings platformSettings;
     NEXUS_PlatformStartServerSettings serverSettings;
     NEXUS_PlatformConfiguration platformConfig;
+    NxClient_JoinSettings joinSettings;
 
     if (daemon(0, 0) < 0) {
         LOGE("nexusinit: FATAL: Daemonise Failed!");
@@ -253,13 +232,12 @@ int main(void)
 
     android::ProcessState::self()->startThreadPool();
 
-#if ((ATP_BUILD==0) && (ANDROID_UNDER_LXC==0))
     property_get("ro.nexus.wake.devname", value, NULL);
     if (strlen(value) > 0) {
         strcpy(value2, "devname=\"");
         strcat(value2, value);
         strcat(value2, "\"");
-        LOGD("nexusinit: ro.nexus.wake.devname=%s", value2);
+        LOGD("nexusinit: ro.nexus.wake.devname=%s", value);
     }
 
     /* insmod wakeup driver first */
@@ -271,21 +249,13 @@ int main(void)
         LOGI("nexusinit: insmod %s succeeded", WAKEUP_DRIVER_FILENAME);
     }
 
-#ifdef NEXUS_MODE_proxy
-    /* Pull the nexus arguments and pass them to the module init */
     property_get("ro.nexus_config", value, NULL);
-
     value2[0] = '\0';
     if (strlen(value) > 0) {
         strcat(value2, "config=");
         strcat(value2, value);
-        LOGD("nexusinit: ro.nexus_config = %s", value2);
+        LOGD("nexusinit: ro.nexus_config = %s", value);
     }
-#else
-    mode_t old_umask = umask(0000);
-    mknod("/dev/brcm0", 0666 | S_IFCHR, makedev(30, 0));
-    umask(old_umask);
-#endif
 
     property_get("ro.nexus.devname", value, NULL);
     if (strlen(value) > 0) {
@@ -295,10 +265,10 @@ int main(void)
         strcat(value2, "devname=\"");
         strcat(value2, value);
         strcat(value2, "\"");
-        LOGD("nexusinit: ro.nexus.devname=%s", value2);
+        LOGD("nexusinit: ro.nexus.devname=%s", value);
     }
 
-    /* insmod nexus.ko driver second */
+    /* insmod nexus driver second */
     if(nexusinit_insmod(NEXUS_DRIVER_FILENAME, value2) != BERR_SUCCESS) {
         LOGE("nexusinit: FATAL: insmod failed on %s!", NEXUS_DRIVER_FILENAME);
         /* failure is FATAL. */
@@ -308,16 +278,22 @@ int main(void)
         LOGI("nexusinit: insmod %s succeeded", NEXUS_DRIVER_FILENAME);
     }
 
-    /* Are we using our own nxserver integrated in to nexus service or not? */
-#ifdef ANDROID_SUPPORTS_NXCLIENT
     struct stat buffer;
-
-    if (stat(NXSERVER_FILENAME, &buffer) == 0) {
-
+    if (stat(NXSERVER_FILENAME, &buffer) == 0)
+    {
         startNxServer();
 
-        /* Make sure we wait a bit before installing the IR input driver */
-        sleep(3);
+        NxClient_GetDefaultJoinSettings(&joinSettings);
+        snprintf(joinSettings.name, NXCLIENT_MAX_NAME, "nexusinit");
+        joinSettings.timeout = 10;
+        /* ... wait for server to be ready. */
+        if (NxClient_Join(&joinSettings) != NEXUS_SUCCESS)
+        {
+            LOGE("nexusinit: FATAL: join failed, is server running?");
+            _exit(1);
+        }
+        /* okay, we are ready... */
+        NxClient_Uninit();
 
         property_set("hw.nexus.platforminit", "on");
 
@@ -325,124 +301,14 @@ int main(void)
             LOGE("nexusinit: FATAL: Could not initialise IR!");
             _exit(1);
         }
-    } else {
+    }
+    else
+    {
         LOGE("nexusinit: FATAL: Could not find \"" NXSERVER_FILENAME "\"!");
         _exit(1);
     }
 
-#else /* ANDROID_SUPPORTS_NXCLIENT */
-    NEXUS_Platform_GetDefaultSettings(&platformSettings);
-
-    platformSettings.mode = NEXUS_ClientMode_eVerified;
-#if ANDROID_SUPPORTS_FRONTEND_SERVICE
-    platformSettings.openFrontend = true;
-#else
-    platformSettings.openFrontend = false;
-#endif
-
-    while (NEXUS_Platform_Init(&platformSettings) != NEXUS_SUCCESS) {
-        LOGE("nexusinit: NEXUS_Platform_Init Failed!");
-        sleep(1);
-    }
-
-    property_set("hw.nexus.platforminit", "on");
-
-    LOGI("nexusinit: NEXUS_Platform_Init Success");
-
-    if (nexusinit_ir() != BERR_SUCCESS) {
-        LOGE("nexusinit: FATAL: Could not initialise IR!");
-        _exit(1);
-    }
-
-#endif /* ANDROID_SUPPORTS_NXCLIENT */
-#else // ATP_BUILD && ANDROID_UNDER_LXC cases
-
-    FILE *f_list;
-    char line[4096];
-    bool bFound = false;
-
-    do
-    {
-        system("ps &> /tmp/proclist.txt");
-
-        f_list = fopen("/tmp/proclist.txt", "r");
-
-        if (!f_list)
-        {
-            LOGE("Failed to open /tmp/proclist.txt");
-            return BERR_UNKNOWN;
-        }
-
-        while (fgets(line, sizeof(line), f_list))
-        {
-            char *p_service;
-
-            p_service = strstr (line, "platform_service");
-
-            if (p_service)
-            {
-                bFound = true;
-                LOGE("platform_service is now running!!");
-                break;
-            }
-
-            else
-            {
-                LOGE("Waiting for platform_service...");
-            }
-        }
-
-        if (f_list)
-            fclose(f_list);
-    }while(!bFound);
-
-    // Wait for a brief moment before installing the nx_ashmem driver
-    sleep(3);
-
-    /* Add the nx_ashmem module which is required for gralloc to function */
-    if (nexusinit_insmod(NX_ASHMEM_DRIVER_FILENAME, "") != BERR_SUCCESS) {
-        LOGE("nexusinit: insmod failed on %s!", NX_ASHMEM_DRIVER_FILENAME);
-        return BERR_UNKNOWN;
-    }
-
-    // Just set this property to unhook Android
-    property_set("hw.nexus.platforminit", "on");
-#endif
-
-#ifdef ANDROID_SUPPORTS_NXCLIENT
     NexusNxService::instantiate();
     LOGI("nexusinit: NexusNxService is now ready");
-#else
-    NexusService::instantiate();
-    LOGI("nexusinit: NexusService is now ready");
-#endif
-
-#if ANDROID_SUPPORTS_FRONTEND_SERVICE
-    NexusFrontendService::instantiate();
-    LOGI("nexusinit: NexusfrontendService is now ready");
-#endif
-
-#ifdef SBS_USES_TRELLIS_INPUT_EVENTS
-    // Frist, create the device node
-    system("busybox mknod /dev/event_write c 34 0");
-    system("busybox chmod 666 /dev/event_write");
-
-    // Now, install the drivers
-    if (nexusinit_insmod(EVENT_FORWARD_DRIVER_FILENAME, "") != BERR_SUCCESS) {
-        LOGE("nexusinit: FATAL: insmod failed on %s!", EVENT_FORWARD_DRIVER_FILENAME);
-        _exit(1);
-    }
-
-    LOGI("nexusinit: insmod succeeded for %s", EVENT_FORWARD_DRIVER_FILENAME);
-
-    if (nexusinit_insmod(EVENT_WRITE_DRIVER_FILENAME, "") != BERR_SUCCESS) {
-        LOGE("nexusinit: FATAL: insmod failed on %s!", EVENT_WRITE_DRIVER_FILENAME);
-        _exit(1);
-    }
-
-    LOGI("nexusinit: insmod succeeded for %s", EVENT_WRITE_DRIVER_FILENAME);
-#endif
-
     android::IPCThreadState::self()->joinThreadPool();
 }
-
