d3e28d10f3e0e2e32df3afa69c79a7b636c97cb3
Harish Vasudeva <hvasudev@broadcom.com>
2014-10-16 16:37:01 -0700

    Adding Tuner support logic. All the code is under the define ENABLE_TUNER
    refs #SWANDROID-1041
    
    Change-Id: I36fc3c10a29ba03833f196d6427b33ecd13ed296

--- bcm_platform/libtv_input/Android.mk
+++ bcm_platform/libtv_input/Android.mk
@@ -13,11 +13,25 @@
 # limitations under the License.
 
 LOCAL_PATH := $(call my-dir)
+NEXUS_LIB=libnexus
 
 include $(CLEAR_VARS)
 
+include $(NEXUS_TOP)/nxclient/include/nxclient.inc
+
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SHARED_LIBRARIES := libcutils liblog
+
+LOCAL_C_INCLUDES += $(NXCLIENT_INCLUDES)
+LOCAL_C_INCLUDES += $(TOP)/vendor/broadcom/bcm_platform/libnexusipc
+LOCAL_C_INCLUDES += $(TOP)/vendor/broadcom/bcm_platform/libnexusservice
+
+LOCAL_CFLAGS += $(NXCLIENT_CFLAGS)
+LOCAL_CFLAGS += $(NEXUS_CFLAGS) $(addprefix -I,$(NEXUS_APP_INCLUDE_PATHS)) $(addprefix -D,$(NEXUS_APP_DEFINES)) -DANDROID $(MP_CFLAGS)
+LOCAL_CFLAGS += -DLOGD=ALOGD -DLOGE=ALOGE -DLOGW=ALOGW -DLOGV=ALOGV -DLOGI=ALOGI
+
+LOCAL_SHARED_LIBRARIES := libcutils liblog libutils $(NEXUS_LIB)
+LOCAL_SHARED_LIBRARIES += libnxclient libnexusipcclient
+
 LOCAL_SRC_FILES := tv_input.cpp
 LOCAL_MODULE := tv_input.bcm_platform
 LOCAL_MODULE_TAGS := optional
