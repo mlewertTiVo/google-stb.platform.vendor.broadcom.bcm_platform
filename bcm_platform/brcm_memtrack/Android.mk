8c41f9ce1262b312f2630cb909a5c02aff2a4051
Pierre Couillaud <pierre@broadcom.com>
2014-10-19 08:51:27 -0700

    memtrack: clean up useless build things.
    refs #SWANDROID-944
    
    Change-Id: I9d18825853328377877cbc6be7d35990f8008ace
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- bcm_platform/brcm_memtrack/Android.mk
+++ bcm_platform/brcm_memtrack/Android.mk
@@ -1,24 +1,5 @@
 LOCAL_PATH := $(call my-dir)
-REFSW_PATH :=${LOCAL_PATH}/../brcm_nexus
-APPLIBS_TOP ?=$(LOCAL_PATH)/../../../../../../../..
-NEXUS_TOP ?= $(LOCAL_PATH)/../../../../../../../../../nexus
-
-# Nexus multi-process, client-server related CFLAGS
-MP_CFLAGS = -DANDROID_CLIENT_SECURITY_MODE=$(ANDROID_CLIENT_SECURITY_MODE)
-
-ifeq ($(NEXUS_MODE),proxy)
-NEXUS_LIB=libnexus
-else
-ifeq ($(NEXUS_WEBCPU),core1_server)
-NEXUS_LIB=libnexus_webcpu
-else
-NEXUS_LIB=libnexus_client
-endif
-endif
-
 include $(CLEAR_VARS)
-
-$(warning NX-BUILD: CFLAGS ${NEXUS_CFLAGS}, CLI-CFLAGS: ${NXCLIENT_CFLAGS}, CLI-INC: ${NXCLIENT_INCLUDES})
 
 LOCAL_SRC_FILES:=  \
     brcm_memtrack.cpp
@@ -28,7 +9,7 @@ LOCAL_CFLAGS:= $(NEXUS_CFLAGS) $(addprefix -I,$(NEXUS_APP_INCLUDE_PATHS)) $(addp
 LOCAL_C_INCLUDES += hardware/libhardware/include
 
 LOCAL_SHARED_LIBRARIES := \
-    $(NEXUS_LIB)
+    libnexus
 
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
