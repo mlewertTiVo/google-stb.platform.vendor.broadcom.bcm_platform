709374952bfe8a3f7de69fbdb9effdb5f2a6e4ca
Harish Vasudeva <hvasudev@broadcom.com>
2014-10-29 22:00:20 -0700

    Getting ready for the re-architecture refs #SWANDROID-1041
    Change-Id: I701da011b632f72419a5111fb2a79bc14f6bce77

--- bcm_platform/libtv_input/tv_input.cpp
+++ bcm_platform/libtv_input/tv_input.cpp
@@ -46,6 +46,9 @@ typedef struct tv_input_private {
     const tv_input_callback_ops_t* callback;
     void* callback_data;
 
+    int iNumConfigs;
+    tv_stream_config_t *s_config;
+
     NexusIPCClientBase *ipcclient;
     NexusClientContext *nexus_client;
     NEXUS_FrontendHandle frontend;
@@ -74,7 +77,7 @@ tv_input_module_t HAL_MODULE_INFO_SYM = {
 };
 
 // Turn this ON for TUNER support
-//#define ENABLE_TUNER
+#define ENABLE_TUNER
 
 #ifdef ENABLE_TUNER
 // native_handle_t related
@@ -118,6 +121,10 @@ static int tv_input_initialize(struct tv_input_device* dev, const tv_input_callb
 	tuner_event.device_info.audio_type = AUDIO_DEVICE_NONE;
 	tuner_event.device_info.audio_address = NULL;
 
+    // Config data
+    priv->iNumConfigs = 1;
+    priv->s_config =  (tv_stream_config_t *) malloc(sizeof (tv_stream_config_t));
+
 	ALOGE("%s: Calling notify", __FUNCTION__);
     callback->notify(dev, &tuner_event, data);
 
@@ -132,26 +139,28 @@ static int tv_input_initialize(struct tv_input_device* dev, const tv_input_callb
 
 static int tv_input_get_stream_configurations(const struct tv_input_device *dev, int dev_id, int *numConfigs, const tv_stream_config_t **s_out)
 {
-    tv_stream_config_t *s_cfg[2];
+    int i;
+    tv_input_private_t* priv = (tv_input_private_t*)dev;
 
 	ALOGE("%s: Enter", __FUNCTION__);
 
-    *numConfigs = 2;
+    // Initialize the # of channels
+    *numConfigs = priv->iNumConfigs;
 
-    s_cfg[0] = (tv_stream_config_t *) malloc(sizeof (tv_stream_config_t));
-    s_cfg[1] = (tv_stream_config_t *) malloc(sizeof (tv_stream_config_t));
+    // Just update 1 stream (represents main video, no PIP for now)
+    if (priv->s_config != NULL)
+    {
+        ALOGE("%s: s_config = %p", __FUNCTION__, priv->s_config);
 
-    s_cfg[0]->stream_id = 100;
-    s_cfg[0]->type = TV_STREAM_TYPE_INDEPENDENT_VIDEO_SOURCE;
-    s_cfg[0]->max_video_width = 1920;
-    s_cfg[0]->max_video_height = 1080;
+        priv->s_config->stream_id = 1;
+        priv->s_config->type = TV_STREAM_TYPE_INDEPENDENT_VIDEO_SOURCE;
+        priv->s_config->max_video_width = 1920;
+        priv->s_config->max_video_height = 1080;
+    }
 
-    s_cfg[1]->stream_id = 200;
-    s_cfg[1]->type = TV_STREAM_TYPE_INDEPENDENT_VIDEO_SOURCE;
-    s_cfg[1]->max_video_width = 1920;
-    s_cfg[1]->max_video_height = 1080;
+    *s_out = (priv->s_config);
 
-    *s_out = s_cfg[0];
+	ALOGE("%s: Exit", __FUNCTION__);
 
     return 0;
 }
@@ -182,20 +191,19 @@ static int tv_input_open_stream(struct tv_input_device *dev, int dev_id, tv_stre
 static int tv_input_close_stream(struct tv_input_device*, int, int)
 {
 	ALOGE("%s: Enter", __FUNCTION__);
-    return -EINVAL;
+    return 0;
 }
 
-static int tv_input_request_capture(
-        struct tv_input_device*, int, int, buffer_handle_t, uint32_t)
+static int tv_input_request_capture(struct tv_input_device*, int, int, buffer_handle_t, uint32_t)
 {
 	ALOGE("%s: Enter", __FUNCTION__);
-    return -EINVAL;
+    return 0;
 }
 
 static int tv_input_cancel_capture(struct tv_input_device*, int, int, uint32_t)
 {
 	ALOGE("%s: Enter", __FUNCTION__);
-    return -EINVAL;
+    return 0;
 }
 
 /*****************************************************************************/
@@ -205,6 +213,7 @@ static int tv_input_device_close(struct hw_device_t *dev)
 	ALOGE("%s: Enter", __FUNCTION__);
     tv_input_private_t* priv = (tv_input_private_t*)dev;
     if (priv) {
+        free(priv->s_config);
         free(priv);
     }
     return 0;
