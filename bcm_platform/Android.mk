7fa6400a4ba78e304e299e8b0645fc4927be0d21
Danny Yip <dannyyip@broadcom.com>
2014-11-03 19:12:24 -0800

    Compile in BcmCoverFlow by default as the application is needed for 4K testing.
    refs #SWANDROID-1232
    
    Change-Id: I8c94a042e67cc2e7ce46f5b4775245dfb98c9271

--- bcm_platform/Android.mk
+++ bcm_platform/Android.mk
@@ -22,6 +22,7 @@ BCM_APPS_PATH := vendor/broadcom/bcm_platform/app
 
 include ${BCM_APPS_PATH}/BcmAdjustScreenOffset/Android.mk
 include ${BCM_APPS_PATH}/BcmChangeDisplayFormat/Android.mk
+include ${BCM_APPS_PATH}/BcmCoverFlow/Android.mk
 include ${BCM_APPS_PATH}/BcmDemoPIP/Android.mk
 include ${BCM_APPS_PATH}/BcmGeneralSTBFunctions/Android.mk
 include ${BCM_APPS_PATH}/BcmHdmiInPlayer/Android.mk
