96c211a6a860c53d497aee47f03c4abd18faf09b
Pierre Couillaud <pierre@broadcom.com>
2014-11-26 12:36:25 -0800

    format: increase /data to its full size (4GB)
    refs #SWANDROID-1010
    
    Change-Id: Ic9149b180f2770a4d74e97f1e3532efcf876694a
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- bcm_platform/tools/UnpackAndroid/format_android_usb_multipart_8GB.sh
+++ bcm_platform/tools/UnpackAndroid/format_android_usb_multipart_8GB.sh
@@ -30,8 +30,8 @@ sgdisk -g -o /dev/$1
 sgdisk -a 1 -n 1:34:8388642 -t 1:0700 -c 1:"kernel" /dev/$1
 sgdisk -a 1 -n 2:8388643:8798243      -c 2:"rootfs" /dev/$1
 sgdisk -a 1 -n 3:8798244:10895396     -c 3:"system" /dev/$1
-sgdisk -a 1 -n 4:10895397:12992549    -c 4:"data"   /dev/$1
-sgdisk -a 1 -n 5:12992550:15089702    -c 5:"cache"  /dev/$1
+sgdisk -a 1 -n 4:10895397:19284005    -c 4:"data"   /dev/$1
+sgdisk -a 1 -n 5:19284006:21381158    -c 5:"cache"  /dev/$1
 
 mkfs.vfat -F 32 -n kernel /dev/${1}1
 mkfs.ext4 -L       rootfs /dev/${1}2
