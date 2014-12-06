507ff3c004f195bbc69b49d3c5e9d9ed347abd85
Edwin Chan <echan@broadcom.com>
2014-12-05 16:28:36 -0800

    stage_host: Allow relative paths to be used
    refs #SWANDROID-987
    
    Change-Id: Ia037e0d302e1e4ff0f8f271392f1f453d002e133
    Signed-off-by: Edwin Chan <echan@broadcom.com>

--- bcm_platform/tools/UnpackAndroid/stage_host.sh
+++ bcm_platform/tools/UnpackAndroid/stage_host.sh
@@ -6,38 +6,42 @@ if [ $# -lt 2 ]
 then
   echo ""
   echo "Usage: stage_host.sh <image_dir> <staging_dir>"
-  echo "           image_dir  : Absolute path to the directory containing boot.img, system.img and userdata.img."
+  echo "           image_dir  : Path to the directory containing boot.img, system.img and userdata.img."
   echo "                        Ex. <workspace>/out/target/product/bcm_platform"
-  echo "           staging_dir: Absolute path to a directory where the filesystem will be staged to in"
+  echo "           staging_dir: Path to a directory where the filesystem will be staged to in"
   echo "                        your Linux machine. Make sure that this directory has NFS access."
   echo ""
   exit
 fi
 
-echo "1) Cleaning up staging dir: $2"
+image_dir="$( cd $1 && pwd )"
+staging_dir="$( cd $2 && pwd )"
+
+
+echo "1) Cleaning up staging dir: $staging_dir"
 echo "   ... removing the boot+bootloaders folder"
-rm -rf $2/boot
-rm -rf $2/loaders
+rm -rf $staging_dir/boot
+rm -rf $staging_dir/loaders
 
 echo "2) Unpacking ramdisk and copy kernel"
-cd $2
-$tools_dir/tools/split_boot $1/boot.img
+cd $staging_dir
+$tools_dir/tools/split_boot $image_dir/boot.img
 mv boot/boot.img-kernel kernel
-cp $1/boot.img boot.img
+cp $image_dir/boot.img boot.img
 
 echo "3) Unsparse ext4's"
-$tools_dir/tools/simg2img $1/system.img ./boot/system.raw.img
-$tools_dir/tools/simg2img $1/userdata.img ./boot/userdata.raw.img
+$tools_dir/tools/simg2img $image_dir/system.img ./boot/system.raw.img
+$tools_dir/tools/simg2img $image_dir/userdata.img ./boot/userdata.raw.img
 
 echo "4) Copying stage_target* scripts."
-cp $tools_dir/stage_target.sh $2
-cp $tools_dir/stage_target_multipart.sh $2
-cp $tools_dir/stage_target_bootimg.sh $2
+cp $tools_dir/stage_target.sh $staging_dir
+cp $tools_dir/stage_target_multipart.sh $staging_dir
+cp $tools_dir/stage_target_bootimg.sh $staging_dir
 
 echo "5) Copying bootloaders."
-mkdir -p $2/loaders
-cp $1/android_bsu.elf $2/loaders
-cp $1/bolt-ba.bin $2/loaders
-cp $1/bolt-bb.bin $2/loaders
+mkdir -p $staging_dir/loaders
+cp $image_dir/android_bsu.elf $staging_dir/loaders
+cp $image_dir/bolt-ba.bin $staging_dir/loaders
+cp $image_dir/bolt-bb.bin $staging_dir/loaders
 
 echo "!!! Done staging !!!" 
