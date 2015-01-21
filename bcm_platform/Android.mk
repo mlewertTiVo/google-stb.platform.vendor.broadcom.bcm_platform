b6a4c86c2ff03c4c0bb9b5fdec4afba4fe46d2a4
Pierre Couillaud <pierre@broadcom.com>
2015-01-21 07:51:50 -0800

    tools: adding a 'clipping' application.
    to test out effects of various clipping on graphics
    plane.
    
    refs #SWANDROID-1616
    
    Change-Id: I07d3a70621c2bf82c46a7f0c2646992d73363e36
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- bcm_platform/Android.mk
+++ bcm_platform/Android.mk
@@ -39,8 +39,9 @@ include vendor/broadcom/bcm_platform/pmlibservice/Android.mk
 include vendor/broadcom/bcm_platform/libpower/Android.mk
 endif
 
-include vendor/broadcom/bcm_platform/tools/cmatool/Android.mk
 include vendor/broadcom/bcm_platform/tools/calcfb/Android.mk
+include vendor/broadcom/bcm_platform/tools/clipping/Android.mk
+include vendor/broadcom/bcm_platform/tools/cmatool/Android.mk
 include vendor/broadcom/bcm_platform/tools/lmkstats/Android.mk
 include vendor/broadcom/bcm_platform/tools/prdy_pes_playback/Android.mk
 
