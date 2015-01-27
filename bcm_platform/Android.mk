882a1b3ddeb8db91e8f20db2bd519ff6fd25cff3
Pierre Couillaud <pierre@broadcom.com>
2015-01-27 14:11:32 -0800

    nxcfg: port the nxclient 'config' app to tool.
    refs #SWANDROID-1680
    
    Change-Id: I6a39a790bef58c465d3657bec408a0d5e01de30c
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- bcm_platform/Android.mk
+++ bcm_platform/Android.mk
@@ -43,6 +43,7 @@ include vendor/broadcom/bcm_platform/tools/calcfb/Android.mk
 include vendor/broadcom/bcm_platform/tools/clipping/Android.mk
 include vendor/broadcom/bcm_platform/tools/cmatool/Android.mk
 include vendor/broadcom/bcm_platform/tools/lmkstats/Android.mk
+include vendor/broadcom/bcm_platform/tools/nxcfg/Android.mk
 include vendor/broadcom/bcm_platform/tools/prdy_pes_playback/Android.mk
 
 ifneq ($(wildcard vendor/broadcom/bcm_platform/not_for_release/README.txt),)
