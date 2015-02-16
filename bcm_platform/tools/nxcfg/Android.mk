ebb4f3830ef76d69c5bfc21b55d3509f8fa60c77
Pierre Couillaud <pierre@broadcom.com>
2015-02-16 07:22:30 -0800

    nxcfg: fix up in 15.1 refsw integration.
    refs #SWANDROID-1680
    
    Change-Id: Ic0d3a58a1fc584221d202165ebb99ec7cd71d1d1
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- bcm_platform/tools/nxcfg/Android.mk
+++ bcm_platform/tools/nxcfg/Android.mk
@@ -7,14 +7,16 @@ LOCAL_SHARED_LIBRARIES += libcutils
 LOCAL_SHARED_LIBRARIES += libnexus
 LOCAL_SHARED_LIBRARIES += libnxclient
 
-LOCAL_C_INCLUDES += $(NXCLIENT_INCLUDES)
+LOCAL_C_INCLUDES += $(NXCLIENT_INCLUDES) \
+                    $(TOP)/vendor/broadcom/refsw/nexus/nxclient/apps/utils
 
 LOCAL_CFLAGS += $(addprefix -I,$(NEXUS_APP_INCLUDE_PATHS))
 LOCAL_CFLAGS += $(addprefix -D,$(NEXUS_APP_DEFINES))
 LOCAL_CFLAGS += $(NXCLIENT_CFLAGS)
 LOCAL_CFLAGS += -DBSTD_CPU_ENDIAN=BSTD_ENDIAN_LITTLE
 
-LOCAL_SRC_FILES := ../../../refsw/nexus/nxclient/apps/config.c
+LOCAL_SRC_FILES := ../../../refsw/nexus/nxclient/apps/config.c \
+                   ../../../refsw/nexus/nxclient/apps/utils/nxapps_cmdline.c
 
 LOCAL_MODULE := nxcfg
 LOCAL_MODULE_TAGS := optional
