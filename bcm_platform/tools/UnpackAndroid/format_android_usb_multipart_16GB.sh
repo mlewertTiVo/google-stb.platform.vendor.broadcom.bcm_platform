104dc44f15568879f7c8d1c2db1a765365c3bfb8
Dominic Lemire <dominicl@broadcom.com>
2015-02-17 13:47:12 -0800

    Revert "Added persistent data block partition"
    This reverts commit 8340cdf719329cdc7faf25a9ee872ff375c25f17.
    
    The newer GTS doesn't complain if the persistent data block feature
    is not enabled (optional). The current implementation is not
    working with SELinux and would require duplicating of the policy
    in order to specify the exact block device node for each profile
    (as opposed to the generic /dev/block/by-name/pst link). We decided
    to remove this optional feature for now.
    
    refs #SWANDROID-1298
    
    Change-Id: I6dd286c4c9ddf5fe63dbd7a5f1bb1f5468b7a8d8

--- bcm_platform/tools/UnpackAndroid/format_android_usb_multipart_16GB.sh
+++ bcm_platform/tools/UnpackAndroid/format_android_usb_multipart_16GB.sh
@@ -43,7 +43,6 @@ data_size=$((    4*1024*1024*1024/bs)) # As per BoardConfig.mk
 cache_size=$((      256*1024*1024/bs)) # As per BoardConfig.mk
 recovery_size=$((   128*1024*1024/bs)) # As per BoardConfig.mk
 hwcfg_size=$((       20*1024*1024/bs)) # Config files, ex Playready drm.bin
-pst_size=$((          1*1024*1024/bs)) # Persistent data (for GTS), arbitrary size
 
 gpt_first=0; gpt_last=$((gpt_first+gpt_size-1))
 kernel_first=$((gpt_last+1)); kernel_last=$((kernel_first+kernel_size-1))
@@ -53,7 +52,6 @@ data_first=$((system_last+1)); data_last=$((data_first+data_size-1))
 cache_first=$((data_last+1)); cache_last=$((cache_first+cache_size-1))
 recovery_first=$((cache_last+1)); recovery_last=$((recovery_first+recovery_size-1))
 hwcfg_first=$((recovery_last+1)); hwcfg_last=$((hwcfg_first+hwcfg_size-1))
-pst_first=$((hwcfg_last+1)); pst_last=$((pst_first+pst_size-1))
 
 sgdisk -n 1:$kernel_first:$kernel_last -t 1:0700 -c 1:"kernel"   /dev/$1
 sgdisk -n 2:$rootfs_first:$rootfs_last           -c 2:"rootfs"   /dev/$1
@@ -62,7 +60,6 @@ sgdisk -n 4:$data_first:$data_last               -c 4:"data"     /dev/$1
 sgdisk -n 5:$cache_first:$cache_last             -c 5:"cache"    /dev/$1
 sgdisk -n 6:$recovery_first:$recovery_last       -c 6:"recovery" /dev/$1
 sgdisk -n 7:$hwcfg_first:$hwcfg_last             -c 7:"hwcfg"    /dev/$1
-sgdisk -n 8:$pst_first:$pst_last                 -c 8:"pst"      /dev/$1
 
 mkfs.vfat -F 32 -n kernel   /dev/${1}1
 mkfs.ext4 -L       rootfs   /dev/${1}2
@@ -74,8 +71,7 @@ mkfs.vfat -F 32 -n recovery /dev/${1}6
 # The old contents will still exist even though we re-created
 # the partition table. Wipe the partitions that will not get
 # overwritten by the Android image later.
-dd if=/dev/zero          of=/dev/${1}7 bs=$bs count=$hwcfg_size
-dd if=/dev/zero          of=/dev/${1}8 bs=$bs count=$pst_size
+dd if=/dev/zero of=/dev/${1}7 bs=$bs count=$hwcfg_size
 
 # Instantiate the hwcfg partition for those that will not
 # be using it, otherwise it will fail to mount and also
