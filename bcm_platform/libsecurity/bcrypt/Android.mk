1e0d6e8db4a57d19b03b61f14a0318e7ef1f6368
Yuichi Aiba <aiba@broadcom.com>
2015-03-05 14:10:10 -0500

    Seclibs: building libbcrypt&libcmndrm_tl from URSR source
    refs #SWANDROID-1840
    We no longer need the prebuilt libbcrypt.so and libcmndrm_tl.so.
    
    Change-Id: I65ead72f77a048442b5dcd2661a674952c84eb98

--- bcm_platform/libsecurity/bcrypt/Android.mk
+++ bcm_platform/libsecurity/bcrypt/Android.mk
@@ -15,9 +15,57 @@
 #-------------
 # libbcrypt.so
 #-------------
-LOCAL_PATH := $(call my-dir)
+LOCAL_PATH := $(call my-dir)/../../../refsw/BSEAV/lib/security/bcrypt/src
 
 include $(CLEAR_VARS)
-LOCAL_PREBUILT_LIBS := ${DRM_BUILD_MODE}/libbcrypt.so
+
+ifeq ($(NEXUS_MODE),proxy)
+NEXUS_LIB=libnexus
+else
+ifeq ($(NEXUS_WEBCPU),core1_server)
+NEXUS_LIB=libnexus_webcpu
+else
+NEXUS_LIB=libnexus_client
+endif
+endif
+
+LOCAL_SRC_FILES := \
+    bcrypt.c \
+    bcrypt_aes_sw.c \
+    bcrypt_aescbc_sw.c \
+    bcrypt_aesecb_sw.c \
+    bcrypt_asn1_sw.c \
+    bcrypt_cmac_sw.c \
+    bcrypt_dbg.c \
+    bcrypt_descbc_sw.c \
+    bcrypt_desecb_sw.c \
+    bcrypt_dh_sw.c \
+    bcrypt_dsa_sw.c \
+    bcrypt_ecc_sw.c \
+    bcrypt_ecdsa_sw.c \
+    bcrypt_md5_sw.c \
+    bcrypt_rc4_sw.c \
+    bcrypt_rng_sw.c \
+    bcrypt_rsa_sw.c \
+    bcrypt_sha1_sw.c \
+    bcrypt_sha256_sw.c \
+    bcrypt_x509_sw.c
+
+LOCAL_C_INCLUDES := \
+    $(TOP)/vendor/broadcom/refsw/BSEAV/lib/security/bcrypt/include \
+    $(TOP)/external/openssl/include \
+    $(NEXUS_APP_INCLUDE_PATHS)
+
+LOCAL_CFLAGS += -DPIC -fpic -DANDROID
+LOCAL_CFLAGS += $(NEXUS_CFLAGS)
+LOCAL_CFLAGS += $(addprefix -D,$(NEXUS_APP_DEFINES))
+LOCAL_CFLAGS += -DBDBG_DEBUG_BUILD=0
+LOCAL_CFLAGS := $(filter-out -DBDBG_DEBUG_BUILD=1,$(LOCAL_CFLAGS))
+
+LOCAL_SHARED_LIBRARIES := libssl libcrypto $(NEXUS_LIB)
+
+LOCAL_MODULE := libbcrypt
 LOCAL_MODULE_TAGS := optional
-include $(BUILD_MULTI_PREBUILT)
+
+include $(BUILD_SHARED_LIBRARY)
+
--- bcm_platform/libsecurity/common_drm/Android.mk
+++ bcm_platform/libsecurity/common_drm/Android.mk
@@ -19,9 +19,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_PREBUILT_LIBS := ${DRM_BUILD_MODE}/libcmndrm.so
-ifeq ($(SAGE_SUPPORT), y)
-LOCAL_PREBUILT_LIBS += ${DRM_BUILD_MODE}/libcmndrm_tl.so
-endif
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_MULTI_PREBUILT)
 
@@ -34,3 +31,87 @@ LOCAL_PREBUILT_LIBS := ${DRM_BUILD_MODE}/libcmndrmprdy.so
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_MULTI_PREBUILT)
 endif
+
+ifeq ($(SAGE_SUPPORT), y)
+#---------------
+# libcmndrm_tl.so for Modular DRM
+#---------------
+LOCAL_PATH := $(LOCAL_PATH)/../../../refsw/BSEAV/lib/security/common_drm
+
+include $(CLEAR_VARS)
+
+# add SAGElib related includes
+include $(LOCAL_PATH)/../../../../magnum/syslib/sagelib/bsagelib_public.inc
+
+############################################################################
+# Go through all <DRM>_SAGE variables from config.inc and include those
+# <drm>.inc files
+#vvvv#####################vvvvvvvvvvvvvvvvvvvvvvv#####################vvvv##
+ifeq ($(findstring $(BCHP_CHIP), 7445 7366 7439 7364), $(BCHP_CHIP))
+include $(LOCAL_PATH)/config/config_zeus4x.inc
+else ifeq ($(findstring $(BCHP_CHIP), 7435), $(BCHP_CHIP))
+include $(LOCAL_PATH)/config/config_zeus30.inc
+endif
+
+include $(LOCAL_PATH)/drm_tl/common_tl/common_tl.inc
+COMMON_DRM_TL_SOURCES += ${COMMON_TL_SOURCES}
+COMMON_DRM_TL_DEFINES += ${COMMON_TL_DEFINES}
+
+######################widevine modular DRM################################
+ifeq (${WVOEMCRYPTO_SAGE},OFF)
+#Cannot build non-TL version of Common DRM library)
+else
+include $(LOCAL_PATH)/drm_tl/wvoemcrypto/wvoemcrypto.inc
+COMMON_DRM_TL_SOURCES += ${WVOEMCRYPTO_SOURCES}
+COMMON_DRM_TL_DEFINES += ${WVOEMCRYPTO_DEFINES}
+endif
+
+#################### NETFLIX ################################
+ifeq (${NETFLIX_SAGE},OFF)
+#Cannot build non-TL version of Common DRM library)
+else
+include $(LOCAL_PATH)/drm_tl/netflix/netflix.inc
+COMMON_DRM_TL_SOURCES += ${NETFLIX_SOURCES}
+COMMON_DRM_TL_DEFINES += ${NETFLIX_DEFINES}
+endif
+
+ifeq ($(NEXUS_MODE),proxy)
+NEXUS_LIB=libnexus
+else
+ifeq ($(NEXUS_WEBCPU),core1_server)
+NEXUS_LIB=libnexus_webcpu
+else
+NEXUS_LIB=libnexus_client
+endif
+endif
+
+LOCAL_SRC_FILES := ${COMMON_DRM_TL_SOURCES}
+
+LOCAL_C_INCLUDES := \
+    $(TOP)/vendor/broadcom/refsw/BSEAV/lib/security/bcrypt/include \
+    $(TOP)/vendor/broadcom/refsw/BSEAV/lib/security/common_crypto/include \
+    $(TOP)/vendor/broadcom/refsw/BSEAV/lib/security/common_drm/include \
+    $(TOP)/vendor/broadcom/refsw/BSEAV/lib/security/common_drm/include/priv \
+    $(TOP)/vendor/broadcom/refsw/BSEAV/lib/security/common_drm/include/tl \
+    $(TOP)/vendor/broadcom/refsw/BSEAV/lib/security/sage/srai/include \
+    $(TOP)/vendor/broadcom/refsw/BSEAV/lib/security/sage/include \
+    $(TOP)/vendor/broadcom/refsw/BSEAV/lib/security/sage/include/private \
+    $(TOP)/vendor/broadcom/refsw/BSEAV/lib/security/sage/platforms/include \
+    $(TOP)/vendor/broadcom/refsw/BSEAV/lib/drmrootfs \
+    $(BSAGELIB_INCLUDES) \
+    $(TOP)/external/openssl/include \
+    $(NEXUS_APP_INCLUDE_PATHS)
+
+LOCAL_CFLAGS += -DPIC -fpic -DANDROID
+LOCAL_CFLAGS += $(NEXUS_CFLAGS) $(COMMON_DRM_TL_DEFINES)
+LOCAL_CFLAGS += $(addprefix -D,$(NEXUS_APP_DEFINES))
+LOCAL_CFLAGS += -DBDBG_DEBUG_BUILD=0
+LOCAL_CFLAGS := $(filter-out -DBDBG_DEBUG_BUILD=1,$(LOCAL_CFLAGS))
+
+LOCAL_SHARED_LIBRARIES := $(NEXUS_LIB) libcmndrm libdrmrootfs libsrai
+
+LOCAL_MODULE := libcmndrm_tl
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
+endif
+
