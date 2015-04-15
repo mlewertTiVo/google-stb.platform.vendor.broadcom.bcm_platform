29cd9b0658208469400997361cb82871d6d87600
Rob McConnell <robertwm@broadcom.com>
2015-04-15 19:25:09 +0100

    SWANDROID-2073: [PM] Interactivity time-out not adhered to when off-state isn't S5
    The logic for determining what to set the interactivity timer to was wrong.  It did
    not take into account the doze timeout value.  The logic has been changed to
    only skip the interactivity timeout if the offstate is set to S5 (like before) AND
    the doze timeout is 0.
    
    refs #SWANDROID-2073
    
    Change-Id: Ic24c08062c5b721acb30b1ba130bd95065b16113

--- bcm_platform/libpower/power.cpp
+++ bcm_platform/libpower/power.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2015 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -541,10 +541,11 @@ static void power_set_interactive(struct power_module *module __unused, int on)
             // Start interactive timer
             struct itimerspec ts;
             int interactiveTimeout;
+            int dozeTimeout = power_get_property_doze_timeout();
 
             // If we want to enter S5 (aka cold boot), then we can enter immediately as
             // the system will begin its slow shutdown process...
-            if (offState == ePowerState_S5) {
+            if (dozeTimeout == 0 && offState == ePowerState_S5) {
                 interactiveTimeout = 0;
                 ts.it_value.tv_nsec = 1;
             }
