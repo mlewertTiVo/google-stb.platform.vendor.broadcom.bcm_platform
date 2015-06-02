65136a39800efd409eb317608e5a772981fd5bcf
Danesh Petigara <dpetigara@broadcom.com>
2015-06-02 11:02:12 -0700

    plat-droid: update kernel location
    refs #SWANDROID-2340
    
    Change-Id: I6880e3d8189a83f3e31e678de9021fc59c18f2ef
    Signed-off-by: Danesh Petigara <dpetigara@broadcom.com>

--- bcm_platform/tools/plat-droid.py
+++ bcm_platform/tools/plat-droid.py
@@ -159,7 +159,7 @@ boardconfig="BoardConfig.mk"
 rmdir_then_mkdir(devicedirectory)
 
 # get the toolchain expected for kernel image and modules build
-run_toolchain='bash -c "cat ./kernel/rootfs/toolchain"'
+run_toolchain='bash -c "cat ./kernel/private/97xxx-bcm/rootfs/toolchain"'
 if verbose:
 	print run_toolchain
 lines = check_output(run_toolchain,shell=True).splitlines()
