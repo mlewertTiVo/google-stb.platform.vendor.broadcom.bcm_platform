006adcae767613d003b6a470a29f68ee578d4aab
Pierre Couillaud <pierre@broadcom.com>
2015-12-22 05:56:59 -0800

    signing: adjust-screen-offset now uses bcmstb key.
    refs #SWANDROID-2676
    
    Change-Id: I3afeef8342d4bd9b558f3718da7b0e7501c7b41e
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- bcm_platform/app/BcmAdjustScreenOffset/Android.mk
+++ bcm_platform/app/BcmAdjustScreenOffset/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_JNI_SHARED_LIBRARIES := libjni_adjustScreenOffset
 LOCAL_PACKAGE_NAME := BcmAdjustScreenOffset
-#LOCAL_CERTIFICATE := platform
+LOCAL_CERTIFICATE := $(BCM_VENDOR_STB_ROOT)/bcm_platform/signing/bcmstb
 include $(BUILD_PACKAGE)
 
 
