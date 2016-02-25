ca7d7ff9d81861d3cf3de5135dc0c7a9a8659229
Pierre Couillaud <pierre.couillaud@broadcom.com>
2016-02-25 08:12:09 -0800

    otpgetchipid: convert application to be a nxclient.
    - this allows to build, load and run the application on a live android
    system (since it is now a nxclient rather than an application trying to
    own nexus which would clash with the android's nxserver).
    - you still need to be 'su' to run this successfully as it needs to be
    a verified client.
    
    refs #SWANDROID-3059
    
    Change-Id: Ic1ee4b6cf549aea2caaaf526cac7110e45eafb78
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- tools/otpgetchipid/Android.mk
+++ tools/otpgetchipid/Android.mk
@@ -3,14 +3,16 @@ include $(CLEAR_VARS)
 
 include $(NEXUS_TOP)/nxclient/include/nxclient.inc
 
+LOCAL_SHARED_LIBRARIES += libcutils
 LOCAL_SHARED_LIBRARIES += libnexus
+LOCAL_SHARED_LIBRARIES += libnxclient
 
 LOCAL_C_INCLUDES += $(NXCLIENT_INCLUDES)
 
 LOCAL_CFLAGS += $(NEXUS_APP_CFLAGS)
 LOCAL_CFLAGS += $(NXCLIENT_CFLAGS)
 
-LOCAL_SRC_FILES := ../../../refsw/nexus/examples/security/otp/nexus/otpgetchipid.c
+LOCAL_SRC_FILES := otpgetchipid.c
 
 LOCAL_MODULE := otpgetchipid
 LOCAL_MODULE_TAGS := optional
