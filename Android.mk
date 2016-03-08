3c60d4d6fb5876a7cf1db8818bb0bdd7dece9035
Pierre Couillaud <pierre.couillaud@broadcom.com>
2016-03-08 05:59:21 -0800

    vendor: build update for N PDK.
    fixup to be inline with mainline codebase
    
    refs #SWANDROID-2981
    
    Change-Id: Ice0bae02ae9c94a2e0147ee97428041724ff5e79

--- Android.mk
+++ Android.mk
@@ -37,10 +37,12 @@ include ${BCM_APPS_PATH}/BcmAdjustScreenOffset/Android.mk
 #include ${BCM_APPS_PATH}/BcmHdmiTvInput/Android.mk
 include ${BCM_APPS_PATH}/BcmKeyInterceptor/Android.mk
 include ${BCM_APPS_PATH}/BcmOtaUpdater/Android.mk
+ifneq ($(TARGET_BUILD_PDK),true)
+include ${BCM_APPS_PATH}/BcmSidebandViewer/Android.mk
+endif
 include ${BCM_APPS_PATH}/BcmSpdifSetting/Android.mk
 include ${BCM_APPS_PATH}/BcmSplash/Android.mk
 ifneq ($(TARGET_BUILD_PDK),true)
-include ${BCM_APPS_PATH}/BcmSidebandViewer/Android.mk
 #include ${BCM_APPS_PATH}/BcmTVInput/Android.mk
 endif
 include ${BCM_APPS_PATH}/BcmTvSettingsLauncher/Android.mk
