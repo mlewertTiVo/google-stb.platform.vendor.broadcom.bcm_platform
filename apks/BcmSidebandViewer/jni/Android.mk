002d230e10aca0d3b73ea960b760e74a243238e0
Pierre Couillaud <pierre@broadcom.com>
2016-03-23 18:54:26 -0700

    build: fix up build missed...
    refs #SWANDROID-2981
    
    Change-Id: Iaaafdfe880485a3e4b083db0711721053ff6c858
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- apks/BcmSidebandViewer/jni/Android.mk
+++ apks/BcmSidebandViewer/jni/Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE:= libbcmsidebandviewer_jni
+LOCAL_C_INCLUDES += $(TOP)/${BCM_VENDOR_STB_ROOT}/bcm_platform/media/libbcmsideband/include
 LOCAL_SRC_FILES:= com_broadcom_sideband_BcmSidebandViewer.cpp
 LOCAL_SHARED_LIBRARIES := \
     libnativehelper \
