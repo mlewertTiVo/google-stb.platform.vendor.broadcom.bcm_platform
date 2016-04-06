b187e8e9a8e720ffe962861cfc5d4f7bf54734f6
Derek Smith <dereks@broadcom.com>
2016-04-06 16:44:46 -0700

    CameraHal: Change advertised camera API support to 1.
    refs #SWANDROID-3137
    
    Change-Id: I1fdaf04f57b2c5cba169da17167b84d90e00fadb
    Signed-off-by: Derek Smith <dereks@broadcom.com>

--- hals/camera2/CameraHal.cpp
+++ hals/camera2/CameraHal.cpp
@@ -31,7 +31,7 @@
 camera_module_t HAL_MODULE_INFO_SYM = {
    .common = {
       .tag                = HARDWARE_MODULE_TAG,
-      .module_api_version = CAMERA_MODULE_API_VERSION_2_0,
+      .module_api_version = CAMERA_MODULE_API_VERSION_1_0,
       .hal_api_version    = HARDWARE_HAL_API_VERSION,
       .id                 = CAMERA_HARDWARE_MODULE_ID,
       .name               = "camera for set-top-box platforms",
