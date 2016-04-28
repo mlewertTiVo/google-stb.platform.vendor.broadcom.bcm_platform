23ab4c4dc75463af8b7932b9e3dcda5dc0c02005
Rob McConnell <Robert.McConnell@broadcom.com>
2016-04-28 16:36:24 +0100

    SWANDROID-3229: [PM] Excessive number of calls to droid_pm_wakeup_ack_status() when system hasn't fully suspended
    Increased the NxClient_GetStandbyStatus() polling time from 100ms to 1s to help
    reduce the number of calls to "droid_pm_wakeup_ack_status()" being made per second.
    
    With this change the number of calls suppressed from printk logging has reduced
    by a factor of 100 (e.g. from 250 to 2).  This has drastically reduced the number
    of process-to-process and user-space/kernel-space context switches.
    
    refs #SWANDROID-3229
    
    Change-Id: I81b5520eafd77ad6e4dc1da6cfc52277fd3a25f3

--- nxif/libnexusipc/nexus_ipc_common.h
+++ nxif/libnexusipc/nexus_ipc_common.h
@@ -62,7 +62,7 @@
 #define NXCLIENT_SERVER_TIMEOUT_IN_MS (500)
 
 /* Define the timeout to wait for polling the standby status. */
-#define NXCLIENT_STANDBY_MONITOR_TIMEOUT_IN_MS  (100)
+#define NXCLIENT_STANDBY_MONITOR_TIMEOUT_IN_MS  (1000)
 
 /* Define the maximum number of client ID's */
 #define CLIENT_MAX_IDS 16
