66769287ace536db40fc8fa960154c4ed0e778dd
Takayuki Hoshi <hoshi@google.com>
2016-07-13 15:47:26 +0900

    Fix send_cec's include path
    Bug: 30106073
    Change-Id: I9dcaee80a0641f3e8223f4935b48f4ab53e2da55

--- tools/cec/Android.mk
+++ tools/cec/Android.mk
@@ -28,7 +28,7 @@ LOCAL_SHARED_LIBRARIES := \
    libnexusipcclient
 
 LOCAL_C_INCLUDES += $(TOP)/${BCM_VENDOR_STB_ROOT}/bcm_platform/libnexusservice \
-                    $(TOP)/${BCM_VENDOR_STB_ROOT}/bcm_platform//libnexusipc
+                    $(TOP)/${BCM_VENDOR_STB_ROOT}/bcm_platform/libnexusipc
 
 LOCAL_CFLAGS := $(NEXUS_APP_CFLAGS)
 LOCAL_CFLAGS += -Wno-multichar -DLOGD=ALOGD -DLOGE=ALOGE -DLOGW=ALOGW -DLOGI=ALOGI -DLOGV=ALOGV
