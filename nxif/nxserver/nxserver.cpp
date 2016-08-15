c4d0b63a8cfaf5aa81fb137541a87471c3ec9fb1
Pierre Couillaud <pierre@broadcom.com>
2016-08-15 05:42:31 -0700

    nxserver: adjust for removed api settings.
    refs #SWSTB-2693
    
    Change-Id: I3f932126c55e03a830a6199ae636c5471de1d561
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- nxif/nxserver/nxserver.cpp
+++ nxif/nxserver/nxserver.cpp
@@ -120,8 +120,6 @@
 #define NX_CAPABLE_FRONT_END           "ro.nx.capable.fe"
 #define NX_NO_OUTPUT_VIDEO             "ro.nx.output.dis"
 
-#define NX_HDMI_DRM_KEY                "ro.nx.hdmi_drm"
-
 #define NX_ODV                         "ro.nx.odv"
 #define NX_ODV_ALT_THRESHOLD           "ro.nx.odv.use.alt"
 #define NX_ODV_ALT_1_USAGE             "ro.nx.odv.a1.use"
@@ -981,14 +979,6 @@ static nxserver_t init_nxserver(void)
           }
        }
        fclose(key);
-    }
-
-    /* -hdmi_drm */
-    memset(value, 0, sizeof(value));
-    if ( property_get(NX_HDMI_DRM_KEY, value, NX_PROP_DISABLED) ) {
-        if (strlen(value) && (strtoul(value, NULL, 0) > 0)) {
-            settings.hdmi.noDrm = false;
-        }
     }
 
     /* -hdcp1x_keys some-key-file */
