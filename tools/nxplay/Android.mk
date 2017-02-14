9c0c0032ad4c436beb385f3a0b00de5fb30cf2d2
Pierre Couillaud <pierre.couillaud@broadcom.com>
2017-02-14 06:30:02 -0800

    nxplay: add include path for glob.h
    refs #SWANDROID-3693
    
    Change-Id: I58d81cb37b1316d2798244f3086c7fcff13aa52d
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- tools/nxplay/Android.mk
+++ tools/nxplay/Android.mk
@@ -1,4 +1,4 @@
-LOCAL_PATH := $(NEXUS_TOP)
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_PATH := $(subst ${ANDROID}/,,$(LOCAL_PATH))
 
@@ -9,6 +9,7 @@ LOCAL_C_INCLUDES += $(NXCLIENT_INCLUDES)
 LOCAL_C_INCLUDES += $(NEXUS_TOP)/nxclient/apps/utils
 LOCAL_C_INCLUDES += $(NEXUS_TOP)/utils
 LOCAL_C_INCLUDES += $(BSEAV_TOP)/lib/tshdrbuilder
+LOCAL_C_INCLUDES += $(BSEAV_TOP)/lib/glob
 LOCAL_C_INCLUDES := $(subst ${ANDROID}/,,$(LOCAL_C_INCLUDES))
 
 LOCAL_CFLAGS += $(NXCLIENT_CFLAGS)
@@ -17,20 +18,26 @@ LOCAL_CFLAGS += $(addprefix -I,$(NEXUS_APP_INCLUDE_PATHS))
 LOCAL_CFLAGS += $(addprefix -D,$(NEXUS_APP_DEFINES))
 
 LOCAL_SHARED_LIBRARIES := libnexus libnxclient
-LOCAL_SRC_FILES := \
-    nxclient/apps/play.c \
-    nxclient/apps/utils/media_player.c \
-    nxclient/apps/utils/media_player_bip.c \
-    nxclient/apps/utils/media_player_ip.c \
-    nxclient/apps/utils/media_probe.c \
-    nxclient/apps/utils/bfile_crypto.c \
-    nxclient/apps/utils/dvr_crypto.c \
-    nxclient/apps/utils/nxapps_cmdline.c \
-    nxclient/apps/utils/live_source.c \
-    nxclient/apps/utils/binput.c \
-    nxclient/apps/utils/bfont.c \
-    nxclient/apps/utils/bgui.c \
-    utils/namevalue.c
+
+LOCAL_PATH := $(TOP)/${BCM_VENDOR_STB_ROOT}/
+NXCLIENT_APPS_UTILS_SRC := \
+   refsw/nexus/nxclient/apps/utils/bfile_crypto.c \
+   refsw/nexus/nxclient/apps/utils/bfont.c \
+   refsw/nexus/nxclient/apps/utils/bgui.c \
+   refsw/nexus/nxclient/apps/utils/binput.c \
+   refsw/nexus/nxclient/apps/utils/brecord_gui.c \
+   refsw/nexus/nxclient/apps/utils/dvr_crypto.c \
+   refsw/nexus/nxclient/apps/utils/live_source.c \
+   refsw/nexus/nxclient/apps/utils/media_player.c \
+   refsw/nexus/nxclient/apps/utils/media_player_ip.c \
+   refsw/nexus/nxclient/apps/utils/media_player_bip.c \
+   refsw/nexus/nxclient/apps/utils/media_probe.c \
+   refsw/nexus/utils/namevalue.c \
+   refsw/nexus/nxclient/apps/utils/nxapps_cmdline.c \
+   refsw/BSEAV/lib/glob/glob.c
+
+LOCAL_SRC_FILES := refsw/nexus/nxclient/apps/play.c
+LOCAL_SRC_FILES += $(NXCLIENT_APPS_UTILS_SRC)
 
 LOCAL_MODULE := nxplay
 LOCAL_MODULE_TAGS := optional
