2b982c3653e6af160f255ea7f50a76f4d16496ee
Dominic Lemire <dominic.lemire@broadcom.com>
2017-02-24 10:53:20 -0800

    tif: include BcmHdmiTvInput in the build
    The HDMI TV input service is now fixed and can be safely included in
    the build. The service will not run by default, because the TV input
    HAL checks the property ro.nx.trim.hdmiin and doesn't notify the
    service if it's equal to 1 (default).
    
    Note: if you want to use the HDMI TV input, you also need to change the
    kernel memory configuration, not just the property.
    
    refs #SWANDROID-3728
    
    Change-Id: I49ecefc290e8bd560d17ec507d9a4f1269d6a215

--- Android.mk
+++ Android.mk
@@ -31,7 +31,7 @@ include ${BCM_VENDOR_STB_ROOT}/bcm_platform/misc/pmlibservice/Android.mk
 # apk's released.
 BCM_APPS_PATH := ${BCM_VENDOR_STB_ROOT}/bcm_platform/apks
 include ${BCM_APPS_PATH}/BcmAdjustScreenOffset/Android.mk
-#include ${BCM_APPS_PATH}/BcmHdmiTvInput/Android.mk
+include ${BCM_APPS_PATH}/BcmHdmiTvInput/Android.mk
 include ${BCM_APPS_PATH}/BcmKeyInterceptor/Android.mk
 include ${BCM_APPS_PATH}/BcmOtaUpdater/Android.mk
 ifneq ($(TARGET_BUILD_PDK),true)
