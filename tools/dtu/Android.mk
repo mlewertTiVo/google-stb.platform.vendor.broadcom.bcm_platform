d7c52007cd66ff093f6b5ab66cf24bd4e933c648
Pierre Couillaud <pierre.couillaud@broadcom.com>
2017-03-08 07:38:27 -0800

    dtutool: adding 'dtu tool' build for android.
    *** /dev/mem is required for this tool to work, re-enable
        in the _defconfig as needed (not available by default).
    
    refs #SWANDROID-3703
    
    Change-Id: I23c44ef4a53c6dab9b3e00002c34e652386725c2
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

---
+++ tools/dtu/Android.mk
@@ -0,0 +1,23 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+include $(NEXUS_TOP)/nxclient/include/nxclient.inc
+include $(TOP)/${BCM_VENDOR_STB_ROOT}/refsw/magnum/basemodules/dtu/bdtu.inc
+
+LOCAL_C_INCLUDES += $(NXCLIENT_INCLUDES) \
+LOCAL_C_INCLUDES := $(subst ${ANDROID}/,,$(LOCAL_C_INCLUDES))
+
+LOCAL_CFLAGS += $(NEXUS_APP_CFLAGS)
+LOCAL_CFLAGS += $(NXCLIENT_CFLAGS)
+
+LOCAL_SHARED_LIBRARIES := libnexus
+
+LOCAL_PATH := $(TOP)/${BCM_VENDOR_STB_ROOT}
+LOCAL_SRC_FILES := refsw/rockford/applications/dtu/dtutool.c
+LOCAL_SRC_FILES += refsw/magnum/basemodules/dtu/src/bdtu.c
+LOCAL_SRC_FILES += refsw/magnum/basemodules/reg/breg_mem.c
+
+LOCAL_MODULE := dtutool
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_EXECUTABLE)
+
