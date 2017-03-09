d303b57bd05fcc922e2647e53af821848175cbbe
Pierre Couillaud <pierre.couillaud@broadcom.com>
2017-03-08 16:46:27 -0800

    dtutool: adding 'dtu tool' build for android (ii): fixup in O.
    refs #SWANDROID-3703
    
    Change-Id: I0037fb1d7ffa969ff8889773ec5bfb3a6b103daf
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- tools/dtu/Android.mk
+++ tools/dtu/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 include $(NEXUS_TOP)/nxclient/include/nxclient.inc
 include $(TOP)/${BCM_VENDOR_STB_ROOT}/refsw/magnum/basemodules/dtu/bdtu.inc
 
-LOCAL_C_INCLUDES += $(NXCLIENT_INCLUDES) \
+LOCAL_C_INCLUDES += $(NXCLIENT_INCLUDES)
 LOCAL_C_INCLUDES := $(subst ${ANDROID}/,,$(LOCAL_C_INCLUDES))
 
 LOCAL_CFLAGS += $(NEXUS_APP_CFLAGS)
