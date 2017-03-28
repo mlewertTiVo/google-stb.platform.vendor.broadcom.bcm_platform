1501af83d96ce8a17d9038a38ec1de394ae152cf
Pierre Couillaud <pierre@broadcom.com>
2017-03-28 13:31:45 -0700

    sage: add manufcaturing tool cross-compiled for android.
    refs #SWANDROID-3895
    
    Change-Id: I2590e8a3bb020ca3b2bd8f07870aa679b4fec54c
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

---
+++ tools/nxsmt/Android.mk
@@ -0,0 +1,27 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+include $(NEXUS_TOP)/nxclient/include/nxclient.inc
+
+LOCAL_SHARED_LIBRARIES += libcutils
+LOCAL_SHARED_LIBRARIES += libnexus
+LOCAL_SHARED_LIBRARIES += libnxclient
+LOCAL_SHARED_LIBRARIES += libsrai
+LOCAL_SHARED_LIBRARIES += libsmt
+
+LOCAL_C_INCLUDES += $(NXCLIENT_INCLUDES) \
+                    $(TOP)/${BCM_VENDOR_STB_ROOT}/refsw/BSEAV/lib/security/sage/manufacturing/include \
+                    $(TOP)/${BCM_VENDOR_STB_ROOT}/refsw/BSEAV/lib/security/sage/manufacturing
+LOCAL_C_INCLUDES := $(subst ${ANDROID}/,,$(LOCAL_C_INCLUDES))
+
+LOCAL_CFLAGS += $(NEXUS_APP_CFLAGS)
+LOCAL_CFLAGS += $(NXCLIENT_CFLAGS)
+LOCAL_CFLAGS += -DUSE_NXCLIENT=1
+
+LOCAL_PATH := $(TOP)/${BCM_VENDOR_STB_ROOT}
+LOCAL_SRC_FILES := refsw/BSEAV/lib/security/sage/manufacturing/app/sage_manufacturing_tool.c
+
+LOCAL_MODULE := nxsmt
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_EXECUTABLE)
+
