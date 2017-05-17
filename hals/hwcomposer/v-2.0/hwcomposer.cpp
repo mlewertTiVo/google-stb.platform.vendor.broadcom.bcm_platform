b5bfcf23eed6d0c99752621d4a998fe09ff32c50
Pierre Couillaud <pierre@broadcom.com>
2017-05-17 08:52:29 -0700

    hwc2: background seeding (ii).
    - fixup case when gpx and video are both present but seeding is still
      required for video to show.
    
    refs #SWANDROID-3987
    
    Change-Id: I18d64e532a5d4ff05f0538b6540eedc5d58e15f1
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- hals/hwcomposer/v-2.0/hwcomposer.cpp
+++ hals/hwcomposer/v-2.0/hwcomposer.cpp
@@ -4380,7 +4380,9 @@ int hwc2_blit_yv12(
    /* first blit check if we need to seed. */
    if (*ms != hwc2_seeding_none) {
       if (!dsp->sfb) {
-         if ((oa.x+oa.width < ss.width) || (oa.y+oa.height < ss.height)) {
+         if ((*ms == hwc2_seeding_vid) ||
+             ((*ms == hwc2_seeding_gfx) &&
+              ((oa.x+oa.width < ss.width) || (oa.y+oa.height < ss.height)))) {
             hwc2_fb_seed(hwc2, d, (*ms == hwc2_seeding_vid) ? HWC2_TRS : HWC2_OPQ);
             hwc2_chkpt(hwc2);
             ALOGI_IF((dsp->lm & LOG_COMP_DEBUG),
@@ -4647,7 +4649,9 @@ int hwc2_blit_gpx(
    /* first blit check if we need to seed. */
    if (*ms != hwc2_seeding_none) {
       if (!dsp->sfb) {
-         if ((oa.x+oa.width < ds.width) || (oa.y+oa.height < ds.height)) {
+         if ((*ms == hwc2_seeding_vid) ||
+             ((*ms == hwc2_seeding_gfx) &&
+              ((oa.x+oa.width < ds.width) || (oa.y+oa.height < ds.height)))) {
             hwc2_fb_seed(hwc2, d, (*ms == hwc2_seeding_vid) ? HWC2_TRS : HWC2_OPQ);
             hwc2_chkpt(hwc2);
             ALOGI_IF((dsp->lm & LOG_COMP_DEBUG),
