10d81c7e202c61ee23cd3dc359b1bbc750e44fa1
Jerry Wong <jerry.wong@broadcom.com>
2017-07-07 01:00:21 -0700

    libbcmsideband: fix up build for master (ii)
    Change-Id: I677120ebc93a6cf32305ebe5c5c2b39726686cd2

--- apks/BcmSidebandViewer/jni/Android.mk
+++ apks/BcmSidebandViewer/jni/Android.mk
@@ -3,7 +3,10 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE:= libbcmsidebandviewer_jni
 LOCAL_C_INCLUDES += $(TOP)/${BCM_VENDOR_STB_ROOT}/bcm_platform/media/libbcmsideband/include \
-                    $(TOP)/${BCM_VENDOR_STB_ROOT}/bcm_platform/media/libbcmsidebandplayer/include/
+                    $(TOP)/${BCM_VENDOR_STB_ROOT}/bcm_platform/media/libbcmsidebandplayer/include \
+                    $(TOP)/frameworks/native/libs/arect/include \
+                    $(TOP)/frameworks/native/libs/nativewindow/include \
+                    $(TOP)/frameworks/native/libs/nativebase/include
 LOCAL_SRC_FILES:= com_broadcom_sideband_BcmSidebandViewer.cpp
 LOCAL_SHARED_LIBRARIES := \
     libnativehelper \
