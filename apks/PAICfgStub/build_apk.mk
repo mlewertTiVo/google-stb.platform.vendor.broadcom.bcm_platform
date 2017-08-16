51fc5eab7e6e6b74d4a305a4bb03d2fdeb5ee39f
Pierre Couillaud <pierre.couillaud@broadcom.com>
2017-08-16 08:46:46 -0700

    vendor: default stub package to exercise PAI.
    refs #SWANDROID-4289
    
    Change-Id: If41ee80862068552eff89e1b5f9f1ea97a8a9790
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

---
+++ apks/PAICfgStub/build_apk.mk
@@ -0,0 +1,19 @@
+LOCAL_MODULE_TAGS := optional
+LOCAL_SDK_VERSION := current
+
+LOCAL_FULL_MANIFEST_FILE := $(LOCAL_PATH)/stub/AndroidManifest.xml
+ifeq ($(apk_variant),)
+  LOCAL_PACKAGE_NAME := PlayAutoInstallConfig
+  LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+else
+  LOCAL_PACKAGE_NAME := PlayAutoInstallConfig-$(apk_variant)
+  LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res-$(apk_variant)
+endif
+
+# We don't have other java source files.
+LOCAL_SOURCE_FILES_ALL_GENERATED := true
+LOCAL_PROGUARD_ENABLED := disabled
+LOCAL_CERTIFICATE := $(BCM_VENDOR_STB_ROOT)/bcm_platform/signing/bcmstb
+
+LOCAL_AAPT_FLAGS += --version-name $(APK_VERSION) --version-code $(APK_VERSION)
+include $(BUILD_PACKAGE)
