c4f585065e26abbfcbf23877333904fda9c4b4b7
Pierre Couillaud <pierre@broadcom.com>
2017-10-08 08:49:55 -0700

    gralloc: link against hw-binder for vndk in treble mode.
    refs #SWANDROID-3750
    
    Change-Id: I7eab29164ed861236fc9cd7cc077af2cb8387e44
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- hals/gralloc/Android.mk
+++ hals/gralloc/Android.mk
@@ -29,13 +29,17 @@ LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SHARED_LIBRARIES := libnexus \
                           liblog \
                           libcutils \
-                          libbinder \
                           libutils \
                           libdl \
                           libnxwrap \
                           libnxbinder \
                           libnxevtsrc \
                           libnxclient
+ifeq ($(LOCAL_DEVICE_FULL_TREBLE),y)
+LOCAL_SHARED_LIBRARIES += libhwbinder
+else
+LOCAL_SHARED_LIBRARIES += libbinder
+endif
 
 LOCAL_C_INCLUDES += $(TOP)/${BCM_VENDOR_STB_ROOT}/drivers/nx_ashmem
 ifeq ($(V3D_VARIANT),)
