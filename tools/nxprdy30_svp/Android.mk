cfe09a765597b7349a78eca0621e7b56b01e9d57
Pierre Couillaud <pierre@broadcom.com>
2017-12-13 06:54:36 -0800

    tools: playready applications must depend on playready support (build fixup).
    refs #SWANDROID-3655
    
    Change-Id: I3900879e59c108263fb5191bbc56b53a0d2c4d6b
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>
    (cherry picked from commit 42fe100850dd797ea51d6e8151cb6a2bee0b43ac)

--- tools/nxprdy30_svp/Android.mk
+++ tools/nxprdy30_svp/Android.mk
@@ -1,3 +1,5 @@
+ifneq ($(ANDROID_SUPPORTS_PLAYREADY), n)
+
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -61,3 +63,6 @@ LOCAL_MODULE := nxprdy30_svp
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_EXECUTABLE)
+
+endif
+
