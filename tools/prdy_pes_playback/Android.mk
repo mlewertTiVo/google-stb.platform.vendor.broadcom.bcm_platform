95dea171306dbd3322fbaa8e44ee1459365eba4d
Dan Willemsen <dwillemsen@google.com>
2017-12-27 13:39:50 -0800

    Remove deprecated ANDROID_BUILD_TOP
    Test: lunch elfin-userdebug; mmma vendor/broadcom/bcm_platform/tools/prdy_pes_playback
    Change-Id: Icad7b631642ec5f0e62d58ce5944ce1e4e55c00d

--- tools/prdy_pes_playback/Android.mk
+++ tools/prdy_pes_playback/Android.mk
@@ -23,28 +23,28 @@ LOCAL_CFLAGS += $(NXCLIENT_CFLAGS)
 LOCAL_CFLAGS += -DLINUX -DB_REFSW_ANDROID
 
 LOCAL_CFLAGS += -DDRM_INLINING_SUPPORTED=1 -DDRM_BUILD_PROFILE=900 -DTARGET_LITTLE_ENDIAN=1 -DTARGET_SUPPORTS_UNALIGNED_DWORD_POINTERS=0
-LOCAL_CFLAGS += -I$(ANDROID_BUILD_TOP)/${BCM_VENDOR_STB_ROOT}/refsw/nexus/extensions/security/usercmd/include/40nm
-LOCAL_CFLAGS += -I$(ANDROID_BUILD_TOP)/${BCM_VENDOR_STB_ROOT}/refsw/nexus/extensions/security/otpmsp/include/40nm
-LOCAL_CFLAGS += -I$(ANDROID_BUILD_TOP)/${BCM_VENDOR_STB_ROOT}/refsw/nexus/extensions/security/keyladder/include/40nm
-LOCAL_CFLAGS += -I$(ANDROID_BUILD_TOP)/${BCM_VENDOR_STB_ROOT}/refsw/nexus/extensions/security/securersa/include
+LOCAL_CFLAGS += -I${BCM_VENDOR_STB_ROOT}/refsw/nexus/extensions/security/usercmd/include/40nm
+LOCAL_CFLAGS += -I${BCM_VENDOR_STB_ROOT}/refsw/nexus/extensions/security/otpmsp/include/40nm
+LOCAL_CFLAGS += -I${BCM_VENDOR_STB_ROOT}/refsw/nexus/extensions/security/keyladder/include/40nm
+LOCAL_CFLAGS += -I${BCM_VENDOR_STB_ROOT}/refsw/nexus/extensions/security/securersa/include
 # drmtypes.h
-LOCAL_CFLAGS += -I$(ANDROID_BUILD_TOP)/${BCM_VENDOR_STB_ROOT}/refsw/BSEAV/lib/playready/2.5/inc
+LOCAL_CFLAGS += -I${BCM_VENDOR_STB_ROOT}/refsw/BSEAV/lib/playready/2.5/inc
 # common_crypto.h
-LOCAL_CFLAGS += -I$(ANDROID_BUILD_TOP)/${BCM_VENDOR_STB_ROOT}/refsw/BSEAV/lib/security/common_crypto/include
+LOCAL_CFLAGS += -I${BCM_VENDOR_STB_ROOT}/refsw/BSEAV/lib/security/common_crypto/include
 # drm_prdy_http.h
-LOCAL_CFLAGS += -I$(ANDROID_BUILD_TOP)/${BCM_VENDOR_STB_ROOT}/refsw/BSEAV/lib/security/common_drm/include
+LOCAL_CFLAGS += -I${BCM_VENDOR_STB_ROOT}/refsw/BSEAV/lib/security/common_drm/include
 # sage_srai.h
-LOCAL_CFLAGS += -I$(ANDROID_BUILD_TOP)/${BCM_VENDOR_STB_ROOT}/refsw/BSEAV/lib/security/sage/srai/include
-LOCAL_CFLAGS += -I$(ANDROID_BUILD_TOP)/${BCM_VENDOR_STB_ROOT}/refsw/magnum/syslib/sagelib/include
+LOCAL_CFLAGS += -I${BCM_VENDOR_STB_ROOT}/refsw/BSEAV/lib/security/sage/srai/include
+LOCAL_CFLAGS += -I${BCM_VENDOR_STB_ROOT}/refsw/magnum/syslib/sagelib/include
 
 # bmp4_util.h
-LOCAL_CFLAGS += -I$(ANDROID_BUILD_TOP)/${BCM_VENDOR_STB_ROOT}/refsw/BSEAV/lib/media
+LOCAL_CFLAGS += -I${BCM_VENDOR_STB_ROOT}/refsw/BSEAV/lib/media
 # bioatom.h
-LOCAL_CFLAGS += -I$(ANDROID_BUILD_TOP)/${BCM_VENDOR_STB_ROOT}/refsw/BSEAV/lib/utils
+LOCAL_CFLAGS += -I${BCM_VENDOR_STB_ROOT}/refsw/BSEAV/lib/utils
 # bmp4_fragment_make_segment.c
-LOCAL_CFLAGS += -I$(ANDROID_BUILD_TOP)/${BCM_VENDOR_STB_ROOT}/refsw/BSEAV/lib/media/build
+LOCAL_CFLAGS += -I${BCM_VENDOR_STB_ROOT}/refsw/BSEAV/lib/media/build
 # nxclient.h
-LOCAL_CFLAGS += -I$(ANDROID_BUILD_TOP)/${BCM_VENDOR_STB_ROOT}/refsw/nexus/nxclient/include
+LOCAL_CFLAGS += -I${BCM_VENDOR_STB_ROOT}/refsw/nexus/nxclient/include
 
 LOCAL_SRC_FILES := \
     pr_piff_playback.c \
