eb1c50a73a44b08e427718dccb1733731bda6b09
Blair Prescott <Blair.Prescott@broadcom.com>
2018-01-24 12:59:13 -0800

    wifi: Add custom wifi HAL
    The default HAL is overriden with a modified version. The original source
    is from hardware/interfaces/wifi/1.0/default, last modified in commit
    0a2ef0302e. The modifications declare some capabilities not supported,
    and return a not supported error from some interfaces. This helps pass
    some VTS tests.
    
    refs #SWANDROID-4147
    
    Change-Id: I4e6d06ddbcc7377c3cc18e1eecab4b02174437f6
    (cherry picked from commit 57b7c67eec3b5a5aca46590c568e4b36951a3d40)

---
+++ hals/wifi/1.0/Android.mk
@@ -0,0 +1,55 @@
+# Copyright (C) 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+LOCAL_PATH := $(call my-dir)
+
+WIFI_HAL_ROOT := ../../../../../../hardware/interfaces/wifi/1.0/default
+include $(CLEAR_VARS)
+LOCAL_MODULE := android.hardware.wifi@1.0-bcm
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_CPPFLAGS := -Wall -Werror -Wextra
+ifdef WIFI_HIDL_FEATURE_AWARE
+LOCAL_CPPFLAGS += -DWIFI_HIDL_FEATURE_AWARE
+endif
+LOCAL_SRC_FILES := \
+    hidl_struct_util.cpp \
+    ${WIFI_HAL_ROOT}/hidl_sync_util.cpp \
+    ${WIFI_HAL_ROOT}/service.cpp \
+    ${WIFI_HAL_ROOT}/wifi.cpp \
+    ${WIFI_HAL_ROOT}/wifi_ap_iface.cpp \
+    wifi_chip.cpp \
+    ${WIFI_HAL_ROOT}/wifi_legacy_hal.cpp \
+    ${WIFI_HAL_ROOT}/wifi_legacy_hal_stubs.cpp \
+    ${WIFI_HAL_ROOT}/wifi_mode_controller.cpp \
+    ${WIFI_HAL_ROOT}/wifi_nan_iface.cpp \
+    ${WIFI_HAL_ROOT}/wifi_p2p_iface.cpp \
+    ${WIFI_HAL_ROOT}/wifi_rtt_controller.cpp \
+    ${WIFI_HAL_ROOT}/wifi_sta_iface.cpp \
+    ${WIFI_HAL_ROOT}/wifi_status_util.cpp
+LOCAL_C_INCLUDES := \
+    ${LOCAL_PATH}/${WIFI_HAL_ROOT}
+
+LOCAL_SHARED_LIBRARIES := \
+    android.hardware.wifi@1.0 \
+    libbase \
+    libcutils \
+    libhidlbase \
+    libhidltransport \
+    liblog \
+    libnl \
+    libutils \
+    libwifi-hal \
+    libwifi-system
+LOCAL_INIT_RC := android.hardware.wifi@1.0-bcm.rc
+include $(BUILD_EXECUTABLE)
