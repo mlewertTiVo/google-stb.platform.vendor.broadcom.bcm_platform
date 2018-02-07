dc0e1ecdf2fb01f108238ef7ecaa95e24fd4cdb0
Pierre Couillaud <pierre@broadcom.com>
2018-02-06 17:51:38 -0800

    tools: move to "priv" things that are not to be released.
    those are unsupported tools which are used for internal validation
    and not maintained consistantly across releases; they are not part
    of the final image.
    
    refs #SWANDROID-4805
    
    Change-Id: I017aca97bdd87dcdff929b55196bbb223d6433dd
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>
    (cherry picked from commit 41072178d2810f6f1361cae75b2863f0ea37af8f)

--- tools/raw2bmp/Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := raw2bmp.c
-LOCAL_MODULE := raw2bmp
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_HOST_EXECUTABLE)
