ae65d33594c9791a58091bfd9ab14ca6b7c602cc
Pierre Couillaud <pierre@broadcom.com>
2018-06-07 11:26:06 -0700

    apks: fix up build for bp3 application.
    refs #SWANDROID-5141
    
    Change-Id: I414ad20e0a929c3fa0bf864a7291a2ab515c3dda
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- apks/BcmBP3Config/Android.mk
+++ apks/BcmBP3Config/Android.mk
@@ -1,16 +1,17 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
+
 LOCAL_RESOURCE_DIR := $(BCM_VENDOR_STB_ROOT)/bcm_platform/tools/brand/res
 LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res
 
-LOCAL_PACKAGE_NAME := BcmBP3Config
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_PRIVATE_PLATFORM_APIS := true
-
 LOCAL_JAVA_LIBRARIES := android.hidl.base-V1.0-java
 LOCAL_STATIC_JAVA_LIBRARIES := bcm.hardware.bp3-V1.0-java-static
+
+LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_PACKAGE_NAME := BcmBP3Config
 LOCAL_CERTIFICATE := $(BCM_VENDOR_STB_ROOT)/bcm_platform/signing/bcmstb
 
 include $(BUILD_PACKAGE)
