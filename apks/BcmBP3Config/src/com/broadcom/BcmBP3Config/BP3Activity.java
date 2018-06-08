14687fc0947161bc04d2fe894d2b1b99c33eb923
Pierre Couillaud <pierre@broadcom.com>
2018-06-08 03:15:46 -0700

    apks: adding "provision" support to bp3 application.
    refs #SWANDROID-5141
    
    Change-Id: I8db85145936ea28b646f4f5d7a0a9610d44a4be6
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- apks/BcmBP3Config/src/com/broadcom/BcmBP3Config/BP3Activity.java
+++ apks/BcmBP3Config/src/com/broadcom/BcmBP3Config/BP3Activity.java
@@ -18,87 +18,165 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.util.Log;
 import android.os.RemoteException;
+import android.os.Handler;
+import android.os.Looper;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+import static com.broadcom.BcmBP3Config.BP3Activity.State.INVALID;
+import static com.broadcom.BcmBP3Config.BP3Activity.State.INIT;
+import static com.broadcom.BcmBP3Config.BP3Activity.State.OPTION_STATUS;
+import static com.broadcom.BcmBP3Config.BP3Activity.State.SERVICE_SEARCH;
+import static com.broadcom.BcmBP3Config.BP3Activity.State.SERVICE_FOUND;
+import static com.broadcom.BcmBP3Config.BP3Activity.State.SERVICE_FAILED;
+import static com.broadcom.BcmBP3Config.BP3Activity.State.INTERFACE_VALID;
+import static com.broadcom.BcmBP3Config.BP3Activity.State.INTERFACE_INVALID;
+import static com.broadcom.BcmBP3Config.BP3Activity.State.STATUS_INVALID;
+import static com.broadcom.BcmBP3Config.BP3Activity.State.STATUS_RETRIEVED;
+import static com.broadcom.BcmBP3Config.BP3Activity.State.OPTION_SERVER;
+import static com.broadcom.BcmBP3Config.BP3Activity.State.SERVER_RUNNING;
+import static com.broadcom.BcmBP3Config.BP3Activity.State.SERVER_STOPPED;
+import static com.broadcom.BcmBP3Config.BP3Activity.State.OPTION_PROVISION;
+import static com.broadcom.BcmBP3Config.BP3Activity.State.PROVISIONED;
+import static com.broadcom.BcmBP3Config.BP3Activity.State.PROVISION_FAILED;
 
 import bcm.hardware.bp3.V1_0.Ibp3;
 
 public class BP3Activity extends Activity
 {
-	private static final String TAG = "BcmBP3Config";
+    private static final String TAG = "BcmBP3Config";
     enum Mode
     {
         STATUS, SERVICE, PROVISION, INVALID
     }
 
-	enum State
-	{		
-		INIT, SERVICE_SEARCH, SERVICE_FOUND, SERVICE_FAILED, INTERFACE_VALID, STATUS_INVALID, STATUS_RETRIEVED
-	}
-
+    enum State
+    {
+	    INVALID, INIT, SERVICE_SEARCH, SERVICE_FOUND, SERVICE_FAILED, INTERFACE_VALID, INTERFACE_INVALID, OPTION_STATUS,
+	    OPTION_SERVER, OPTION_PROVISION, STATUS_INVALID, STATUS_RETRIEVED, SERVER_RUNNING, SERVER_STOPPED, PROVISIONED, PROVISION_FAILED
+    }
     Context mContext;
     private RadioGroup mRadioGroup;
     private RadioButton mStatusButton, mServerButton, mProvisionButton;
     private TextView mInfo1, mInfo2;
     private EditText mArgs1, mArgs2;
     private Button mRunButton;
-
     private String strStatusResult = "N/A";
-	/* = "Broadcom - H264/AVC\n" +
-                                        "Broadcom - MPEG-2\n" +
-                                        "Broadcom - AVS\n" +
-                                        "Broadcom - H265 (HEVC)\n" +
-                                        "Broadcom - VP9\n" +
-                                        "Broadcom - CA Multi2\n" +
-                                        "Broadcom - CA DVB-CSA3\n" +
-                                        "Dolby - Post Proc: DAP\n" +
-                                        "Dolby - Decode Dolby Digital\n" +
-                                        "Dolby - Decode Dolby Digital Plus\n" +
-                                        "Dolby - Decode AC4\n" +
-                                        "Dolby - Decode TrueHD\n" +
-                                        "Dolby - MS10/11\n" +
-                                        "Dolby - MS12 v1\n" +
-                                        "Dolby - MS12 v2\n" +
-                                        "DTS - DTS TruVolume\n" +
-                                        "DTS - DTS Digital Surround\n" +
-                                        "DTS - DTS-HD (M6)\n" +
-                                        "DTS - DTS-HDMA (M8)\n" +
-                                        "DTS - DTS Headphone:X\n" +
-                                        "DTS - DTS Virtual:X\n" +
-                                        "DTS - DTS:X";*/
     private String strStatusInfo = "Displays the currently enabled licenses\n";
     private String strServerInfo = "IP Address (xx.xx.xx.xx)";
     private String strKeyInfo = "Authentication Key:";
     private String strLicenseInfo = "Licenses (ex: 3,4):";
     private String strPortInfo = "Port (default is 80):";
-
+    private String iMessage = "INVALID";
     private String strStatusArgs = "N/A";
     private String strServerArgs = "";
     private String strKey = "", strLicenses = "";
     private Mode iMode = Mode.STATUS;
-	private State iState = State.INIT;
+    private State iState = INIT, iPrevState = INVALID;
     private String strRun = "Run App", strStop = "Stop App";
     private RelativeLayout.LayoutParams mParams1;
-
+    private boolean bRun = true;
     SharedPreferences sharedPref;
     SharedPreferences.Editor editor;
+    private Ibp3 bp3Interface;
 
-	private Ibp3 bp3Interface;
+    private void bp3Thread(final Context appContext)
+    {
+        new Thread()
+        {
+            public void run() 
+            {
+                while (bRun) 
+                {
+                    try 
+                    {
+                        runOnUiThread(new Runnable() 
+                        {
+                            @Override
+                            public void run() 
+                            {
+                                if (iPrevState != iState)
+                                {
+                                    // Reset to null
+                                    iMessage = "";
+
+                                    if (iState == INIT)
+                                        iMessage = "Initializing";
+
+                                    else if (iState == SERVICE_SEARCH)
+                                        iMessage = "Searching for the service";
+
+                                    else if (iState == SERVICE_FAILED)
+                                        iMessage = "Failed to find the service";
+
+                                    else if (iState == SERVICE_FOUND)
+                                        iMessage = "Found the service";
+
+                                    else if (iState == INTERFACE_INVALID)
+                                        iMessage = "Invalid interface";
+
+                                    else if (iState == INTERFACE_VALID)
+                                        iMessage = "The interface is valid";
+
+                                    else if (iState == OPTION_STATUS)
+                                        iMessage = "Will check for the status";
+
+                                    else if (iState == STATUS_INVALID)
+                                        iMessage = "Status invalid";
+
+                                    else if (iState == STATUS_RETRIEVED)
+                                        iMessage = "Status retrieved";
+
+                                    else if (iState == OPTION_SERVER)
+                                        iMessage = "Will run in server mode";
+
+                                    else if (iState == SERVER_STOPPED)
+                                        iMessage = "Server stopped";
+
+                                    else if (iState == SERVER_RUNNING)
+                                        iMessage = "Server running";
+
+                                    else if (iState == OPTION_PROVISION)
+                                        iMessage = "Will provision the box";
+
+                                    else if (iState == PROVISION_FAILED)
+                                        iMessage = "Failed to provision with the new licenses";
+
+                                    else if (iState == PROVISIONED)
+                                        iMessage = "Successfully provisioned the box with the new Licenses";
+
+                                    // Display the message (if needed)
+                                    if (!iMessage.isEmpty())
+                                        Toast.makeText(appContext, iMessage, Toast.LENGTH_SHORT).show();
+
+                                    iPrevState = iState;
+                                }
+                            } // run()
+                        }); // runOnUiThread()
+                    Thread.sleep(1000);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }   // while
+            }   // run()
+        }.start();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bp3);
-//        Toast.makeText(this,"In main page...", Toast.LENGTH_LONG).show();
 
         mContext= this.getApplicationContext();
-//        sharedPref = PreferenceManager.getDefaultSharedPreferences(mContext);
-//        editor = sharedPref.edit();
 
-		iState = State.INIT;
+        // Kick off the thread
+        bp3Thread(mContext);
+
+        iState = INIT;
 
         // Get the widgets reference from XML layout
         mRadioGroup = (RadioGroup) findViewById(R.id.idRadioGroup);
@@ -132,37 +210,34 @@ public class BP3Activity extends Activity
         mInfo2.setVisibility(View.INVISIBLE);
         mArgs2.setVisibility(View.INVISIBLE);
 
-		iState = State.SERVICE_SEARCH;
-		Log.w(TAG, "iState = SERVICE_SEARCH");
+        iState = SERVICE_SEARCH;
 
-		// Get the service handle from the HAL
-		try {
-           bp3Interface = Ibp3.getService();
+        // Get the service handle from the HAL
+        try {
+            bp3Interface = Ibp3.getService();
         } catch (RemoteException e) {
-			iState = State.SERVICE_FAILED;
-//		   Toast.makeText(mContext, "RemoteException trying to reach Ibp3", Toast.LENGTH_LONG).show();
-           Log.w(TAG, "RemoteException trying to reach Ibp3", e);
-           return;
+            iState = SERVICE_FAILED;
+            Log.w(TAG, "RemoteException trying to reach Ibp3", e);
+            return;
         }
 
-		iState = State.SERVICE_FOUND;
-		Log.w(TAG, "iState = SERVICE_FOUND");
+        iState = SERVICE_FOUND;
 
         if (bp3Interface == null) {
-//		   Toast.makeText(mContext, "BP3 service extension is null", Toast.LENGTH_LONG).show();
-		   Log.w(TAG, "BP3 service extension is null");
-           throw new IllegalArgumentException("BP3 service extension is null");
+            iState = INTERFACE_INVALID;
+            Log.w(TAG, "BP3 service extension is null");
+            throw new IllegalArgumentException("BP3 service extension is null");
         }
 
-		iState = State.INTERFACE_VALID;
-		Log.w(TAG, "iState = INTERFACE_VALID");
+        iState = INTERFACE_VALID;
 
         mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 // checkedId is the RadioButton selected
                 if (checkedId == R.id.idStatusButton) {
-//                    Toast.makeText(group.getContext(),"idStatusButton...", Toast.LENGTH_LONG).show();
+                    iState = OPTION_STATUS;
+
                     mParams1.height = getResources().getDimensionPixelSize(R.dimen.large_height);
                     mInfo1.setLayoutParams(mParams1);
                     mInfo1.setText(strStatusInfo);
@@ -176,7 +251,8 @@ public class BP3Activity extends Activity
                     mArgs2.setVisibility(View.INVISIBLE);
                     iMode = Mode.STATUS;
                 } else if (checkedId == R.id.idServerButton) {
-//                    Toast.makeText(group.getContext(),"idServerButton...", Toast.LENGTH_LONG).show();
+                    iState = OPTION_SERVER;
+
                     mParams1.height = getResources().getDimensionPixelSize(R.dimen.small_height);
                     mInfo1.setText(strServerInfo);
 
@@ -194,9 +270,7 @@ public class BP3Activity extends Activity
 
                     iMode = Mode.SERVICE;
                 } else if (checkedId == R.id.idProvisionButton) {
-//                    Toast.makeText(group.getContext(),"idProvisionButton...", Toast.LENGTH_LONG).show();
-
-//                    Toast.makeText(group.getContext(),"Authentication-Key: " + sharedPref.getString("AuthKey", "invalid"), Toast.LENGTH_LONG).show();
+                    iState = OPTION_PROVISION;
 
                     mParams1.height = getResources().getDimensionPixelSize(R.dimen.small_height);
                     mInfo1.setLayoutParams(mParams1);
@@ -206,10 +280,9 @@ public class BP3Activity extends Activity
                     mArgs1.setText(strKey);
                     mArgs1.setEnabled(true);
 
-//                    editor.putString("AuthKey", "brcm1234").apply();
-
-//                    mArgs1.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-//                    mArgs1.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    // Enable this to hide the authentication keys
+                    // mArgs1.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    // mArgs1.setTransformationMethod(PasswordTransformationMethod.getInstance());
 
                     mInfo2.setVisibility(View.VISIBLE);
                     mArgs2.setVisibility(View.VISIBLE);
@@ -224,44 +297,36 @@ public class BP3Activity extends Activity
         });
 
         mRunButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+                public void onClick(View v) {
                 String buttonStr = mRunButton.getText().toString();
-
-//                Toast.makeText(v.getContext(),"Mode = " +iMode, Toast.LENGTH_LONG).show();
 
                 switch (iMode) {
                     // Retrieving the currently enabled licenses
                     case STATUS:
                     default:
-//                        Toast.makeText(v.getContext(), "Retrieving the status...", Toast.LENGTH_LONG).show();
+                    try {
+                        strStatusResult = bp3Interface.status();
+                    } catch (RemoteException e) {
+                        iState = STATUS_INVALID;
+                        Log.w(TAG, "RemoteException calling Ibp3.status ", e);
+                        return;
+                    }
 
-						try {
-						  strStatusResult = bp3Interface.status();
-						} catch (RemoteException e) {
-						   iState = State.STATUS_INVALID;
-						   Log.w(TAG, "RemoteException calling Ibp3.status ", e);
-						   return;
-						}
+                    iState = STATUS_RETRIEVED;
+                    Log.w(TAG, "strStatusResult = " + strStatusResult);
 
-						iState = State.STATUS_RETRIEVED;
-						Log.w(TAG, "iState = STATUS_RETRIEVED");
-
-						Log.w(TAG, "strStatusResult = " + strStatusResult);
-
-						if (!strStatusResult.isEmpty())
-							mInfo1.setText(strStatusResult);
-						else
-							mInfo1.setText("N/A");
-                        break;
+                    if (!strStatusResult.isEmpty())
+                        mInfo1.setText(strStatusResult);
+                    else
+                        mInfo1.setText("N/A");
+                    break;
 
                     // Run as a service
                     case SERVICE:
                         // Check if we need to stop the service
-                        if (buttonStr.equals(strStop)) {
-//                            Toast.makeText(v.getContext(), "Stopping the service...", Toast.LENGTH_LONG).show();
-
+                        if (buttonStr.equals(strStop)) 
+                        {
                             // Stop the service now
-                            // TBD
 
                             // Change the button text to Run
                             mRunButton.setText(strRun);
@@ -270,13 +335,13 @@ public class BP3Activity extends Activity
                             mStatusButton.setEnabled(true);
                             mProvisionButton.setEnabled(true);
                             mArgs1.setEnabled(true);
+
+                            iState = SERVER_STOPPED;
                         }
 
                         // Or, if we need to run in Service mode
                         else //if (iMode == Mode.SERVICE)
                         {
-//                            Toast.makeText(v.getContext(), "Starting the service...", Toast.LENGTH_LONG).show();
-
                             // Change the button text to Stop
                             mRunButton.setText(strStop);
 
@@ -284,15 +349,33 @@ public class BP3Activity extends Activity
                             mStatusButton.setEnabled(false);
                             mProvisionButton.setEnabled(false);
                             mArgs1.setEnabled(false);
+
+                            iState = SERVER_RUNNING;
                         }
-                        break;
+                    break;
 
                     // Provisioning the box with new licenses
                     case PROVISION:
-//                        Toast.makeText(v.getContext(), "Provisioning the box...", Toast.LENGTH_LONG).show();
-                        break;
+                        int iResult;
+
+                        StringTokenizer st = new StringTokenizer(mArgs2.getText().toString(), ",");
+                        ArrayList<Integer> licList = new ArrayList<Integer>();
+
+                        while (st.hasMoreTokens())
+                            licList.add(Integer.parseInt(st.nextToken()));
+
+                        try {
+                            iResult = bp3Interface.provision("bp3.broadcom.com", mArgs1.getText().toString(), licList);
+                        } catch (RemoteException e) {
+                            iState = PROVISION_FAILED;
+                            Log.w(TAG, "RemoteException calling Ibp3.status ", e);
+                            return;
+                        }
+
+                        iState = PROVISIONED;
+                    break;
+                    }
                 }
-            }
-        });
-    }
+            });
+        }
 }
