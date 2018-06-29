4bba873cd24e7a778297afa6089425fe0026320b
Pierre Couillaud <pierre.couillaud@broadcom.com>
2018-06-29 10:14:40 -0700

    Revert "Revert "recovery: decouple recovery (platorm) from nexus props (vendor)""
    refs #SWANDROID-5230
    
    This reverts commit 376a1d9228a3fe9017e8e611cbc6bf42358f1831.
    
    Change-Id: I87d46fa888318b22151fac62a1148b0ae7c6f472
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- nxif/nxmini/nxmini.cpp
+++ nxif/nxmini/nxmini.cpp
@@ -352,7 +352,7 @@ int main(void)
     }
 
     /* trigger waiter on nexus server initialization. */
-    property_set("dyn.nx.state", "loaded");
+    property_set("sys.recov.nx.state", "loaded");
 
     /* loop forever. */
     while (1) {
