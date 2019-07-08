938108d5d0e5803e2701f9516dcb52e9e093faa0
Frank Ni <yung-kang.ni@broadcom.com>
2019-07-08 14:48:26 -0700

    Revert "Reverted SWANDROID-6425 and SWANDROID-6582 in prep for a re-apply from dependencies"
    This reverts commit 1f7dbb207fa45e086bb0ddc19abad05e0a200279.
    
    Revert "Resolve merge error from backport."
    
    This reverts commit 2feefd99204b55c238f636c940203dd54b683a86.
    
    Revert "BOMX: Trigger output format change event correctly"
    
    This reverts commit 85ba74c7699538c5e6011ca7deec394d20d48b74.

--- media/libstagefrighthw/bomx_audio_decoder.cpp
+++ media/libstagefrighthw/bomx_audio_decoder.cpp
@@ -3931,7 +3931,7 @@ void BOMX_AudioDecoder::RemoveOutputBuffers()
         return;
     }
 
-    ALOGV("%d Removing %u output buffers from nexus, queue %u", m_instanceNum, numFrames, m_pAudioPorts[1]->QueueDepth());
+    ALOGV("%d Removing %u output buffers from nexus", m_instanceNum, numFrames);
 
     for ( i = 0; i < numFrames; i++ )
     {
@@ -3984,13 +3984,10 @@ void BOMX_AudioDecoder::PollDecodedFrames()
             if ( m_pFrameStatus[i].sampleRate != m_sampleRate && m_pFrameStatus[i].filledBytes > 0 )
             {
                 ALOGI("Sample rate change %u->%u", m_sampleRate, m_pFrameStatus[i].sampleRate);
-                m_formatChangePending = (m_sampleRate != 0) ? true : false;
+                m_formatChangePending = true;
                 m_sampleRate = m_pFrameStatus[i].sampleRate;
-                if ( m_formatChangePending )
-                {
-                    PortFormatChanged(m_pAudioPorts[1]);
-                    return;
-                }
+                PortFormatChanged(m_pAudioPorts[1]);
+                return;
             }
 
             pBuffer = m_pAudioPorts[1]->FindBuffer(FindBufferFromBlock, (void *)m_pMemoryBlocks[i]);
